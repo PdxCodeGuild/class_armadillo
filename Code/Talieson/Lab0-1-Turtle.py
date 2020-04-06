52aea95b20648cf89eadf832a8636448227ee8ce
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-04-06 14:41:04 -0700

    Here is my stick figure!

---
+++ Code/Talieson/Lab0-1-Turtle.py
@@ -0,0 +1,65 @@
+from turtle import *
+
+#draw head
+penup()
+goto(0,80)
+pendown()
+circle(40)
+
+#draw neck
+right(90)
+forward(30)
+
+#draw arms
+right(45)
+forward(40)
+left(45)
+forward(40)
+
+penup()
+goto(0,50)
+pendown()
+left(45)
+forward(40)
+left(45)
+forward(40)
+
+#onto the rest of the body!
+
+penup()
+goto(0,50)
+pendown()
+right(90)
+forward(50)
+
+#legs
+right(30)
+forward(50)
+left(30)
+forward(50)
+
+penup()
+goto(0,50)
+pendown()
+forward(50)
+left(30)
+forward(50)
+right(30)
+forward(50)
+
+#make him a face
+penup()
+goto(-20,120)
+pendown()
+circle (7)
+penup()
+goto(15,120)
+pendown()
+circle(7)
+
+penup()
+goto(20,100)
+pendown()
+goto(0, 95)
+
+done()
