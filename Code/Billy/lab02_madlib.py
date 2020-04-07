2aeb5d9bd5b0452a685951cc4827a8fbf2d5e051
Billy Barnett Jr <bbnett2@billy-barnett-jrs-macbook-pro.local>
2020-04-07 09:32:39 -0700

    edited lab 02 MADLIBS

--- Code/Billy/lab02_madlib.py
+++ Code/Billy/lab02_madlib.py
@@ -23,30 +23,3 @@ elif cont != 'yes':
      print('GOOD BYE...have a nice day!')
         
     
-
-    # cont_2 = ' '
-    # while cont_2 != 'no':
-    #     cont_2 = input('Would you like to try again with different words? ')
-    #     if cont_2 != 'yes':
-    #         break
-    #     print('*'*80)
-    #     print(f'''It's close to {holiday} and something evil's lurkin' in the {noun}. 
-    #     Under the {thing} you see a sight that almost stops your {body_part1}.
-    #     You try to {verb1} but terror takes the sound before you make it.
-    #     You start to {verb2} as horror looks you right between the {body_part2},
-    #     You're paralyzed!''')
-    #     print('*'*80)
-
-
-
-
-# emoticon_gen = " "
-# while emoticon_gen != "no":
-#     emoticon_gen = input("Would you like to generate more random emoticons? ")
-#     if emoticon_gen != "yes":
-#        break  
-#     for x in range(5):
-#         random_eyes = random.choice(eyes) 
-#         random_nose = random.choice(nose)
-#         random_mouth = random.choice(mouth)
-#         print(random_eyes + random_nose + random_mouth)
