b5a4e35aa7a97083a730a2933b33d2e0146636f0
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-04-08 10:46:18 -0700

    Updated with version 3, option to do vertical emoticons.

--- Code/Talieson/Lab05-emoticonGen.py
+++ Code/Talieson/Lab05-emoticonGen.py
@@ -8,16 +8,25 @@ amount = 0
 while True:
     prompt = input("Do you want to know how you're feeling today? (Y/N) ")
     if prompt == "Y":
-        print("I'll give you 5 options to pick from!")
-        time.sleep(1)
-        break
+        vert = input("Do you want vertical or horizonal emoticons? (V/H) ")
+        if vert == "H":
+            print("I'll give you 5 options to pick from!")
+            time.sleep(1)
+            break
+        elif vert == "V":
+            print("I'll give you 5 options to pick from!")
+            time.sleep(1)
+            break
+        else:
+            prompt = False
+            print("I'm sorry that's not a valid resposne.")
     elif prompt == "N":
         print("Then why did you even come here?")
         exit()
     else:
         print("I'm sorry that's not a valid response.")
 
-while prompt == "Y" and amount < 5:
+while prompt == "Y" and amount < 5 and vert == "H":
     randEyes = (":", "=", "8", ";", "|", "B")
     randNose = ("^", "-", "")
     randMouth = ("D", ")", "(", "[", "]", "P", "S")
@@ -26,4 +35,13 @@ while prompt == "Y" and amount < 5:
     mouth = random.choice(randMouth)
 
     print(f"{eyes}{nose}{mouth}")
+    amount += 1
+
+while prompt == "Y" and amount < 5 and vert == "V":
+    randEyes = ("^", "@", "*", "-")
+    randMouth = ("_", ".", "~")
+    eyes = random.choice(randEyes)
+    mouth = random.choice(randMouth)
+
+    print(f"{eyes}{mouth}{eyes}")
     amount += 1
