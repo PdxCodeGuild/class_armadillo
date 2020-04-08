87473efc0783ef42e9b68cbc04b056f1ec57a7c9
Qijoty Ardent Savoy <theardentqijoty@gmail.com>
2020-04-08 10:51:51 -0700

    finished!

--- Code/Qijoty/lab05-emoticon.py
+++ Code/Qijoty/lab05-emoticon.py
@@ -11,36 +11,37 @@
     make the emoticon display the emotion of the user
     generate horizontal or vertical
 '''
+i = 0
+while i < 1:
+    import random
 
-import random
+    eyes = [':', ';', 'x', '8']
+    nose = ['-','.', 'v', '^', '*', '@']
+    mouth = ['3', '>', ')', '$', 'o']
 
-eyes = [':', ';', 'x', '8']
-nose = ['-','.', 'v', '^', '*', '@']
-mouth = ['3', '>', ')', '$', 'o']
+    emotion = input('Are you feeling happy, sad, angry or something else today? ')
 
-emotion = input('Are you feeling happy, sad, angry or something else today? ')
+    if emotion == "happy":
+        print(':-)))')
+    elif emotion == "sad":
+        print(':-[[[')
+    elif emotion == "angry":
+        print('>:{\{\{')
+    else:
+        print(random.choice(eyes) + random.choice(nose) + random.choice(mouth))
 
-if emotion == "happy":
-    print(':-)))')
-elif emotion == "sad":
-    print(':-[[[')
-elif emotion == "angry":
-    print('>:{\{\{')
-else:
-    print(random.choice(eyes) + random.choice(nose) + random.choice(mouth))
+    while True:
+        user_input = input("would you like another emoticon? ").lower()
+        if user_input == "yes":
+            print(random.choice(eyes) + random.choice(nose) + random.choice(mouth))
+        else:
+            print('okie dokie, byeee')
+    '''
+    for emoticon in range(5):
+        face = random.choice(eyes) + random.choice(nose) + random.choice(mouth)
+        print(face)
+    '''
 
-while True:
-    another == input("would you like another emoticon? ").lower()
-if another == "yes":
-    print(random.choice(eyes) + random.choice(nose) + random.choice(mouth))
-'''
-for emoticon in range(5):
+
     face = random.choice(eyes) + random.choice(nose) + random.choice(mouth)
     print(face)
-'''
-
-    else:
-        another == "no":
-     break
-face = random.choice(eyes) + random.choice(nose) + random.choice(mouth)
-print(face)
