12e7104ee25917e1639c5137f4134714da69b60a
Jon <53983746+Carlock1609@users.noreply.github.com>
2020-04-09 11:25:26 -0700

    RPS example

--- Code/Jon/rps.py
+++ Code/Jon/rps.py
@@ -8,17 +8,17 @@ winning = [
 
 while True:
     user_input = input('\nPlease enter (rock, paper, scissors): ')
-    computer_move = random.choice(['rock', 'scissors', 'paper'])
+    computer_move = random.choice(['rock', 'paper', 'scissors'])
 
     for i in winning:
         if user_input == i[0] and computer_move == i[1]:
-            print(f'You won! User chose {user_input} and computer chose {computer_move}.')
-            break
+            print(f'You Won! User chose {user_input} and Computer chose {computer_move}.')
+
         if computer_move == i[0] and user_input == i[1]:
-            print(f'You Lost! User chose {user_input} and computer chose {computer_move}.')
-            break
+            print(f'You Lost! User chose {user_input} and Computer chose {computer_move}.')
+
         if user_input == computer_move:
-            print(f'Tie! User chose {user_input} and computer chose {computer_move}.')
+            print(f'Tie! User chose {user_input} and Computer chose {computer_move}.')
             break
 
     play_again = input('\nDo you want to play again? (y or n): ')
