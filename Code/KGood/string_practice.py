368f893cab1e1594230dc3f3b329473fbd0d4bbd
dirtTastesGood <keegood8@gmail.com>
2020-04-10 13:36:40 -0700

    string manipulation with list comprehensions

---
+++ Code/KGood/string_practice.py
@@ -0,0 +1,21 @@
+def double_letters(word):
+    '''
+    Double every character in a string and return the result
+    '''
+    return ''.join([a*2 for a in word])
+
+# print(double_letters("hello"))
+
+def latest_letter(word):
+    return sorted(word)[-1]
+
+# print(latest_letter("pneumonoultramicroscopicsilicovolcanoconiosis"))
+
+def count_letter(letter, word):
+    '''
+    Return the number of instances of a letter in a string
+    '''
+    return len([char for char in word if char == letter])
+
+# print(count_letter("i", "pneumonoultramicroscopicsilicovolcanoconiosis"))
+# print(count_letter("l", "hello"))
