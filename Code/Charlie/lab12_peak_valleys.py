3a655e8685d069f1defaefd530352b9d76355229
Charlie Villanueva <infidel12@hotmail.com>
2020-04-14 09:31:06 -0700

    lab 12

--- Code/Charlie/lab12_peak_valleys.py
+++ Code/Charlie/lab12_peak_valleys.py
@@ -1,33 +1,46 @@
 
+# data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+ 
 
-#       0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20
-data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+def peaks(data):
+    output = []
+    for i in range(1, len(data))-1:
+
+        if data[1] > data[i-1] and data [i] > data[i+1]:
+            output.append(i)
+    return output
 
 
-# iterating over the elements
-# for element in data:
-#   print(element)
 
-# iterating over a range
-# for i in range(10) # 0, 1, 2, ... 9
-# for i in range(20) # 0, 1, 2, ... 19
-# for i in range(0, 10) # 0, 1, 2, ... 9
-# for llama in range(5, 10) # 5, 6, 7, 8, 9
+def valleys(data):
+    output = []
+    for i in range(1, len(data))-1:
 
-# iterating over the indices
-# for i in range(len(data)):
-#   print(data[i])
+        if data[1] < data[i-1] and data [i] < data[i+1]:
+            output.append(i)
+    return output
 
-# example - adding of numbers
-def sum_pairs(nums):
-  output = []
-  # crashes because i+1 will be one past the end
-  # for i in range(0, len(nums)):
-  for i in range(0, len(nums)-1):
-    print(i, nums[i])
-    total = nums[i] + nums[i+1]
-    output.append(total)
-  return output
+def peaks_and_valleys(data):
+    output_peaks_valleys = []
+    for i in range(1, len(data)-1):
+        if data[i-1]<data[i]>data[i+1] or data[i-1]>data[i]<data[i+1]:
+            output_peaks_valleys.append(i)
+    return output_peaks_valleys
 
-print(sum_pairs([1, 2, 3, 4])) # [3, 5, 7]
-print(sum_pairs([6, 1, 4])) # [7, 5]
+
+data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
+print(peaks(data))
+print(valleys(data))
+print(peaks_and_valleys(data))
+
+
+
+for value in data:
+    print('x'*value)
+
+for i in range(len(data)):
+    for j in range(data[i]):
+        print('x', end='')
+    print()
+
+
