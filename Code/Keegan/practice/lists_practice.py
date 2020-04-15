fef6672516060fb82ad350c81009ad28c1bd7770
dirtTastesGood <keegood8@gmail.com>
2020-04-15 10:47:45 -0700

    update list practice

--- Code/Keegan/practice/lists_practice.py
+++ Code/Keegan/practice/lists_practice.py
@@ -76,19 +76,98 @@ def print_every_other_with_while(nums):
     Print out every other element of a list using a while loop.
     '''
     #  split a list at every other element nums[0:-1:2] => list_name[first_index : last_index : step]  
+    every_other = []
     index = 0
     while index < len(nums):
         
-        if index % 2 == 1:  # if the current value of index is odd
-            del nums[index]  # delete the value from the list at the index
+        if index % 2 == 0:  # if the current value of index is even
+            every_other.append(nums[index])  # [0, 2, 4, 6, 8]
+            
+            # del nums[index]  # delete the value from the list at the index.
+            # deleting from the original list doesn't work in this case
+            # because it changes the length of the list over which we are iterating,
+            # therefore giving us weird results
         
         index += 1  
 
-    print(nums)  # after the loop is ended, print nums list
+    print(every_other)  # after the loop is ended, print nums list
+    return
 
-# print_every_other_with_while([0, 1, 2, 3, 4, 5, 6, 7, 8])    
-# def print_every_other_with_for(nums):
-    # '''
-    # Print out every other element of a list using a for loop.
-    # '''
-#     pass
+# print_every_other_with_while([0, 1, 2, 3, 4, 5, 6, 7, 8]) 
+# num_list = [random.randint(1,100) for i in range(1,10)]  # list comprehension for creating a list with 10 random integers between 1 and 10
+# print_every_other_with_while(num_list)
+# -------------------------- #
+# April 15, 2020
+
+# same as above but with a for loop
+def print_every_other_with_for(nums):
+
+    new_list = []  # create an empty list to hold our new values
+    for i in range(0, len(nums), 2):  # loop through every other index of our original list, using range(start, stop, step=2)
+        new_list.append(nums[i]) # append the value at every other index to our new list
+
+    print(new_list)  # print the list at the end of the loop
+    return
+
+# print_every_other_with_for([0, 1, 2, 3, 4, 5, 6, 7, 8][::-1]) # [::-1] at the end of a list will reverse it
+# num_list = [random.randint(1,100) for i in range(0,10)]  # list comprehension for creating a list with 10 random integers between 1 and 10
+# print(num_list)
+# print_every_other_with_while(num_list)
+
+
+# Problem 5
+
+def reverse(nums):
+    #return nums[::-1] # fancy split notation
+    # nums.reverse() # fance list method to reverse a list in place 
+    # return nums
+ 
+    new_list = [] # holder for new list
+    for i in range(len(nums) - 1, -1, -1):  # start the range at the end of the list, count down to the beginning, subtracting 1 each time
+        new_list.append(nums[i])  # add the element from the original list at the current index to the new list
+    
+    return new_list
+
+# num_list = [1, 2, 3]
+# print(reverse(num_list))
+
+# Problem 6
+
+def extract_less_than_ten(nums):
+
+    less_than_ten = [] # empty list for storing new values
+
+    # loop through each element in the list
+    for index in range(len(nums)): 
+        num = nums[index]  # variable for the current value of the list at the current index
+        if num < 10: # if the value is num is less than 10, add it to the new list
+            less_than_ten.append(num)  
+
+    return less_than_ten
+
+# nums_list = [random.randint(1,100) for i in range(0,100)]
+# print(nums_list)
+# nums_list = [2, 8, 12, 18]
+# print(extract_less_than_ten(nums_list)) # [2, 8]
+
+
+# Problem 7
+
+def common_elements(nums1, nums2):
+    # return set(nums1).intersection(nums2)  # fancy way using sets, though doesn't work with multiple recurring values in the lists
+
+    # find the smaller of the two lists
+    if len(nums1) < len(nums2):
+        short_list = nums1
+    else:
+        short_list = nums2 
+        
+    # check each value of the smaller list to see if it was in the larger
+
+    # if it is, add to new list
+
+
+nums1 = [1, 2, 3, 2]
+nums2 = [2, 3, 4, 3]
+
+print(common_elements(nums1, nums2))
