86fae024b2e7d6a33dc1e4ba0502c665c1735785
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-04-16 11:40:41 -0700

    added practice questions mob

---
+++ Code/Troy/practice_questions.py
@@ -0,0 +1,49 @@
+'''Problem 12
+Write a function that takes n as a parameter, and 
+returns a list containing the first n Fibonacci Numbers.'''
+
+def fibonacci(n):
+    
+    nums = [1, 1]
+    for i in range(2,n):
+    print(i, i-1, i-2)
+        nums.append(nums[i-1] + nums[i-2]) 
+    return(nums)
+
+print(fibonacci(15)) # [1, 1, 2, 3, 5, 8, 13, 21]
+
+'''Write functions to find the minimum, maximum, mean, and (optionally) mode of a list of numbers.
+
+def minimum(nums):
+    ...
+
+def maxmimum(nums):
+    ...
+
+def mean(nums):
+    ...
+
+def mode(nums): # (OPTIONAL)
+    ...'''
+
+def minimum(nums):
+    nums.sort()
+    return nums[0]
+        
+print(minimum([2, 1, 3]))
+
+
+def maximum(nums):
+    nums.sort()
+    return nums[-1]
+
+print(maximum([2, 1, 3]))
+
+def mean(nums):
+    total = 0
+    for i in range(len(nums)):
+        total += nums[i]
+    total = total / len(nums)
+    return total
+
+print(mean([2, 1, 3]))   
