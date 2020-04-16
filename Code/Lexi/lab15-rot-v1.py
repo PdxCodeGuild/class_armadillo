3b51c406a55db39fccef482976aaab3adc66dbe2
lexiconPDX <chaungemail@gmail.com>
2020-04-16 14:38:40 -0700

    lab 15

--- Code/Lexi/lab15-rot-v1.py
+++ Code/Lexi/lab15-rot-v1.py
@@ -1,54 +1,21 @@
 # v1
+# prompts the user for a string, and encodes it with ROT13.
+user_input = input("What is the string? : ")
+#sally
 
-def rot13(text):
-  ...
+def rot13(input): # define the function
+  alphabet = 'abcdefghijklmnopqrstuvwxyz' # original_list
+  rot_alphabet = alphabet[13:] + alphabet[:13] #changed_list
+  for char in range(len(user_input)):
+        #iterate through 'sally'
+    print(char, end = ' ')
+        #create index for alphabet AND rot_alphabet
+    index = alphabet.index(char)
+    #take the index and convert that
+    if char == index:
 
-print(rot13('hello')) # uryyb
+  return input
+  
 
-# v2
-
-def rotn(text, n):
-  ...
-
-print(rotn('hello', 13)) # uryyb
-
-
-# option 1
-# use .find to look up the index of a character in the alphabet
-# use that same index to look up the corresponding character in the rotated alphabet
-# add the rotated character to the output
-char = 'j'
-#           0123456789
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-#           nopqrstuvwxyzabcdefghijklm
-rot_alphabet = alphabet[13:] + alphabet[:13]
-print(rot_alphabet)
-index = alphabet.index(char)
-print(index)
-print(rot_alphabet[index])
-
-# option 2
-# use .find to look up the index of a character in the alphabet
-# add 13 to the index, and mod it so it wraps back around
-# e.g. the index of 'r' is 18
-# (18 + 13)%26 == 5
-
-
-# option 3
-# use ord to find the ascii code of a character
-# subtract 97 so the sequence starts at 0
-# http://www.asciitable.com/
-print(ord('a')) # 97
-print(ord('b')) # 98
-
-
-
-# option 4
-# hard-code a dictionary, use it to look up the rotated character
-{'a': 'n', 'b': 'o', 'c': 'p'}
-
-
-# option 5
-# generate a dictionary?
-
+# INTENDED RESULTprint(rot13('hello')) # uryyb
 
