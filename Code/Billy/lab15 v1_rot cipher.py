7fbf91e1ae9935dba0f2234ee08c8f088a26d434
Billy Barnett <bbarnett2@gmail.com>
2020-04-16 18:27:58 -0700

    edited lab15 v1 (ROT Cipher)

--- Code/Billy/lab15 v1_rot cipher.py
+++ Code/Billy/lab15 v1_rot cipher.py
@@ -3,8 +3,12 @@ def rot13(text):
     output = '' # makes empty output list
     for x in text: # iterates through input one character(chr)(char) at a time
         ascii_code = ord(x) # changes chr to ascii
-        ascii_code -= 97 # lower case, starts sequence at 0
-        output += chr((ascii_code + 13)%26 + 97) # rotates, shifts, changes ascii to chr, adds to ouput variable   
+        if 97 <= ascii_code <= 122: # lower case
+            ascii_code -= 97 # starts sequence at 0
+            output += chr((ascii_code + 13)%26 + 97) 
+            # rotates, shifts, changes ascii to chr, adds to ouput variable
+        else:
+            print(f'{x} is an invalid entry...could not encrypt that character.') # input validation  
     return output
 
 print(rot13('hello')) # uryyb
