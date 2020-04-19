c5153d9b4b700709d9cb91853bbd1d3b42811c6a
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-04-19 16:32:33 -0700

    added lab 09 with comments

--- Code/Troy/lab09 unit converter.py
+++ Code/Troy/lab09 unit converter.py
@@ -8,37 +8,40 @@ import math
 import time
 
 
-# what each distance is by feet
+# what each distance is by feet.
 distance = {'feet': 1, 'meters': 0.3048} 
 
-# len of ft is the distance of 1 ft
+# len of ft is the distance of 1 ft.
 len_of_feet = distance['feet']
 
-# len of m is the distance of 1 m
+# len of m is the distance of 1 m.
 len_of_meters = distance['meters']
 
 # has the user ask the distance
 user_distance = input('What is the distance in feet?: ')
 
-# the input of the user converting it to a number
+# the input of the user converting it to a number.
 user_distance = int(user_distance)
 
-# the input multiplied by the distance
+# the input multiplied by the distance.
 total = user_distance * distance['meters']
 
-# the output of the equation
+# the output of the equation.
 print(f'The grand total is...{total} meters!')
 
 
 '''Version 2 - 4'''
 
-# asks the user what distance is
+# asks the user what distance is.
 user_distance = int(input('What is the distance? '))
-# asks the user what unit of measurement they would like converted
+
+# asks the user what unit of measurement they would like converted.
 user_input = input('What unit would you like to convert? ')
-# asks the user what unit of measurement they would like the previous answer converted to 
+
+# asks the user what unit of measurement they would like the previous answer converted to .
 user_output = input('What unit do you want it converted to? ')
-# distance conversions chart
+
+# distance conversions chart.
 distance_conversions = {'inches': 0.0254,
                         'feet': 0.3048,
                         'yards': 0.9144,
@@ -47,7 +50,8 @@ distance_conversions = {'inches': 0.0254,
                         'kilometers': 1000.0
 } 
 
-# formula to convert one unit of measurement to another unit of measurement
+# formula to convert one unit of measurement to another unit of measurement.
 total = user_distance * (distance_conversions[user_input]  /distance_conversions[user_output])
-# prints the user output in the new unit of measurement
+
+# prints the user output in the new unit of measurement.
 print(f'That is equal to...{total}.')
