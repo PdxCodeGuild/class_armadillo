444be8d4b60c8762bcaabedfae1925e1eeee2a43
lexiconPDX <chaungemail@gmail.com>
2020-04-20 16:49:16 -0700

    strings practice

---
+++ Code/Lexi/ex-pra-s1.py
@@ -0,0 +1,15 @@
+def set_lowercase(strings):
+    newList = []
+    for name in strings:
+        newList.append(name.lower())
+    print(newList)  
+
+
+# WE CAN USE A FOR LOOP TO Convert input strings
+#  to lowercase without any surrounding whitespace.
+
+
+# OR
+
+# WE CAN USE A METHOD .lower()
+
