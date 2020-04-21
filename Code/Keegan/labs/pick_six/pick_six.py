e499cb26a71f34ebf6a981ecb064aaab326aaae4
dirtTastesGood <keegood8@gmail.com>
2020-04-21 14:57:00 -0700

    satiate the linter!

--- Code/Keegan/labs/pick_six/pick_six.py
+++ Code/Keegan/labs/pick_six/pick_six.py
@@ -2,16 +2,18 @@
 # April 21, 2020
 import random
 
+
 def pick_6():
     '''
     Return a list of six random numbers between 1 - 99
     '''
-    return [random.randint(1,99) for i in range(6)]
+    return [random.randint(1, 99) for i in range(6)]
 
 
 def find_matching_nums(winner, ticket):
     '''
-    Check a ticket against the winning numbers and return how many numbers match
+    Check a ticket against the winning numbers
+    and return how many numbers match
     '''
 
     # counter for number of matches
@@ -19,16 +21,18 @@ def find_matching_nums(winner, ticket):
 
     # loop through the length of the ticket
     for i in range(len(ticket)):
-        # check each element in ticket against the corresponding index in winner
+        # check each element in ticket against the
+        # corresponding index in winner
         if ticket[i] == winner[i]:
             # if the numbers are the same, increment counter
             counter += 1
-    
+
     return counter
+
 
 def calculate_payout(number_of_matches):
     '''
-    Return monetary winnings based on the number of matches 
+    Return monetary winnings based on the number of matches
     '''
     payouts = {
         0: 0,
@@ -41,6 +45,7 @@ def calculate_payout(number_of_matches):
     }
 
     return payouts[number_of_matches]
+
 
 def cost_of_habit(balance, expenses):
     '''
@@ -58,13 +63,14 @@ def main():
 
     # Start your balance at 0
     balance = 0
-    
+
     # start earnings and expenses at 0
     earnings = 0
     expenses = 0
 
     ticket_count = 0
-    ### Loop 100,000 times, for each loop:
+
+    # Loop 100,000 times, for each loop:
     while ticket_count < 1e5:
         # Generate a list of 6 random numbers representing the ticket
         ticket = pick_6()
@@ -75,7 +81,7 @@ def main():
 
         # Find how many numbers match
         matches = find_matching_nums(winner, ticket)
-        
+
         # Add to your balance the winnings from your matches
         payout = calculate_payout(matches)
         # balance += payout
@@ -86,7 +92,7 @@ def main():
         # print(f"{matches = }")
         # print(f"{payout  = }")
 
-    expenses = ticket_count * ticket_cost  
+    expenses = ticket_count * ticket_cost
     balance = earnings - expenses
 
     # probably don't need a function for this
@@ -96,5 +102,6 @@ def main():
     # After the loop, print the final balance
     print(f"Your final balance was: ${balance}")
     print(f"Your habit cost you {roi * 100}% of your investment")
+
 
 main()
