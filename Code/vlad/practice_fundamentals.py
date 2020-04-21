57b6430a283d1d9d8ca18ecf5c64afa080515af1
vladimirantigua <vladimirantigua@gmail.com>
2020-04-21 12:29:38 -0700

    Practice 1: Fundamentals April 21 2020

---
+++ Code/vlad/practice_fundamentals.py
@@ -0,0 +1,5 @@
+#Practice 1: Fundamentals April 21 2020 Keegan Group: saved under file fundametals_practice.py under Keegan GitHub
+
+#https://github.com/PdxCodeGuild/class_armadillo/blob/master/1%20Python/practice/practice01-fundamentals.md
+
+
