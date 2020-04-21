e248a2578842aadd999dd9a47cea73312dec207d
Russell Rockwood <russell.rockwood@gmail.com>
2020-04-21 09:41:25 -0700

    added lab 15 v2

---
+++ Code/Russell/lab15_rot_cypher_v2.py
@@ -0,0 +1,20 @@
+
+
+def rotn(text, n):
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    rot_alphabet = ''
+
+    text = input('what is your text?')
+    n = int(input('what is the rotation amount? '))
+​    for char in text:
+       pass
+
+# ​
+# print(rotn(text, n))
+
+# print(rotn('hello', 13)) # uryyb
+# print(rotn('hello', 2)) # jgnnq
+# print(rotn('jgnnq', -2)) # hello
+
+# rot_alphabet = alphabet
