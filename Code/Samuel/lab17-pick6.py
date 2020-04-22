d7a1ff416a1e9cd3fb0c56971ee6209e9396ccdf
Samuel Purdy <spurdy885@gmail.com>
2020-04-22 12:06:33 -0700

    Added comments

--- Code/Samuel/lab17-pick6.py
+++ Code/Samuel/lab17-pick6.py
@@ -1,13 +1,24 @@
+# PDX Code Guild Fullstack Course
+# Lab 17 Pick 6
+# Samuel Purdy
+# 4/22/2020
+
 import random
 
+# Generates a list of 6 random integers and returns it
 def get_ticket():
     ticket = list()
     for i in range(6):
         ticket.append(random.randint(1, 99))
     return ticket
 
+# Returns a value equal to the winnings earned if there 
+# are multiple matches between the ticket given and the 
+# winning numbers
 def get_winnings(ticket, winning_ticket):
     matches = 0
+    # Compares the values of each index since both lists 
+    # are of the same length.
     for i in range(len(winning_ticket)):
         if ticket[i] == winning_ticket[i]:
             matches += 1
@@ -25,9 +36,14 @@ def get_winnings(ticket, winning_ticket):
         return 25000000
     return -2
 
+
 balance = 0
+# Generates a ticket that will win and what is used to 
+# compare the purchased ticket against.
 winning_ticket = get_ticket()
 
+# Loops 100000 times and incriments the ending balance 
+# with whatever winnings are earned.
 for i in range(100000):
     balance += get_winnings(get_ticket(), winning_ticket)
 
