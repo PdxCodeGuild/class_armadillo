296c7b66ed304a0b0906480db908ca33b5f480b9
Adam Meier <meieradamn@gmail.com>
2020-04-22 14:10:34 -0700

    update lab names

---
+++ Code/Adam/Lab18_count_words/lab18_count_words.py
@@ -0,0 +1,37 @@
+"""
+
+Lab 18: Count Words =====================================================================
+
+Let's write a python module to analyze a given text file containing a book for its 
+vocabulary frequency and display the most frequent words to the user in the terminal. 
+Remember there isn't any "perfect" way to identify a word in english (acronymns, mr/ms, 
+contractions, etc), try to find rules that work best.
+
+Find a book on Project Gutenberg. Download it as a UTF-8 text file.
+
+"""
+
+
+import requests
+
+
+"""
+1. Get the text from the file, via "with open..." or using requests
+"""
+
+
+"""
+2.  Make everything lowercase, strip punctuation, split into a list of words.
+"""
+
+
+"""
+3.  Your dictionary will have words as keys and counts as values. If a word isn't in your 
+    dictionary yet, add it with a count of 1. If it is, increment its count.
+"""
+
+
+"""
+4.  Print the most frequent top 10 out with their counts. You can do that with the code below.
+"""
+
