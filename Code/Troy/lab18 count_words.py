03e81d9f05edb940af9193644c3ac2c5780e9c8b
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-04-22 14:43:22 -0700

    lab 18 updated comments

--- Code/Troy/lab18 count_words.py
+++ Code/Troy/lab18 count_words.py
@@ -1,3 +1,7 @@
+# Lab 18 Count Words
+# Troy Fitzgerald
+# 22 April 2020
+
 '''Let's write a python module to analyze a given text file containing a book 
 for its vocabulary frequency and display the most frequent words to the user in 
 the terminal. Remember there isn't any "perfect" way to identify a word in 
@@ -13,9 +17,12 @@ your dictionary yet, add it with a count of 1. If it is, increment its count.
 Print the most frequent top 10 out with their counts. You can do that with the 
 code below.
 
-
-# word_dict is a dictionary where the key is the word and the value is the count'''
-
+# word_dict is a dictionary where the key is the word and the value is the count
+words = list(word_dict.items()) # .items() returns a list of tuples
+words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+    print(words[i])'''
+    
 # imports the module.
 import requests
 import string
