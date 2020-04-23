9f30a8dbbf5f4270a7aed114d10ae9076373b291
Desi0930 <pacifichik24@yahoo.com>
2020-04-23 16:05:48 -0700

    lab06-random-pswd-v1a.py

--- Code/Desi/lab06-random-pswd-v2.py
+++ Code/Desi/lab06-random-pswd-v2.py
@@ -4,16 +4,18 @@
 import string
 import random
 
-# this is a yes/no question for user
-user = input("can you help me generate a password with random characters? y/n: ")
 
-# this line let's you specifically pick a number
-digits = int(input("I would like you to choose a number between 5 and 35: "))
+while True:
+    # this is a yes/no question for user
+    user = input("can you help me generate a password with random characters? y/n: ")
 
-# Returns all lowercase letters, uppercase letters, and digits
-stuff = (string.ascii_lowercase + string.ascii_uppercase + string.digits)
+    # this line let's you specifically pick a number
+    digits = int(input("I would like you to choose a number between 5 and 35: "))
 
-# empty string joined with random.sample beginning with stuff for the amount of digits
-cell_password = "".join(random.sample(stuff, digits))
+    # Returns all lowercase letters, uppercase letters, and digits
+    stuff = (string.ascii_lowercase + string.ascii_uppercase + string.digits)
 
-print (cell_password)
+    # empty string joined with random.sample beginning with stuff for the amount of digits
+    cell_password = "".join(random.sample(stuff, digits))
+
+    print (cell_password)
