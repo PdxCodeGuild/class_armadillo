7e976b15d7cb3ff371c2dc0808e9ff3f04276fd5
vladimirantigua <vladimirantigua@gmail.com>
2020-04-23 18:10:02 -0700

    Lab 18: Count Words V2

--- Code/vlad/lab18_countwords_v2.py
+++ Code/vlad/lab18_countwords_v2.py
@@ -82,27 +82,38 @@ new_dict = dict ()
 # nested if statements within the forloop that checks whether that word is already in the dictioanry
 # If it is, add value += 1
 # if the word is not in the dictionary, add word as key, set value to `1`
-for word in enumerate(list_of_words): 
-        print(word)
-        # Check if the word is already in dictionary 
-        if word in new_dict: 
-            # Increment count of word by 1 if the word is already in the dictionary
-            new_dict[word] = new_dict[word] + 1
-        else: 
-            # Add the word to dictionary with count 1 if the word is not in the dictionary
-            new_dict[word] = 1
+#tuple unpacking = for i, word in enumerate(list_of_words):
 
-#print(new_dict) 
+#Version 2
+# Count how often each unique pair of words is used, 
+# then print the top 10 most common pairs with their counts.
+
+for i, word in enumerate(list_of_words): 
+        print(i,word)
+        if i >= len(list_of_words)-1:
+            break
+            
+        next_word = list_of_words[i+1]
+        word_plus_one = f'{word} {next_word}'
+        # Check if the word is already in dictionary 
+        if word_plus_one in new_dict: 
+            # Increment count of word by 1 if the word is already in the dictionary
+            new_dict[word_plus_one] = new_dict[word_plus_one] + 1
+        else: 
+            # Add the word_plus_one to dictionary with count 1 if the word_plus_one is not in the dictionary
+            new_dict[word_plus_one] = 1
+print(list_of_words[4467])
+print(new_dict) 
 #Print the contents of dictionary 
 # for key in list(new_dict.keys()): 
 #    print(key, ":", new_dict[key]) 
 
 # word_dict is a dictionary where the key is the word and the value is the count
-# words = list(new_dict.items()) # .items() returns a list of tuples
-# print(words)
-# words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
-# for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
-#     print(words[i])
+words = list(new_dict.items()) # .items() returns a list of tuples
+print(words)
+words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+    print(words[i])
 
 # >>> list = ['a','b','c']
 # >>> list
