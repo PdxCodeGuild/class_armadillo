c4921cd8472abb1af992167964c03943b6ac89d2
jhurdle7 <63259028+jhurdle7@users.noreply.github.com>
2020-04-23 17:28:29 -0700

    finised lab 13 version 2

--- Code/Jai/lab02madlib.py
+++ Code/Jai/lab02madlib.py
@@ -1,14 +1,13 @@
-keep_running = 'yes' 
-while keep_running = 'yes':
-    
-plural_noun1 = input("give me a plural noun")
-plural_noun2 = input("give me a plural noun")
-verb = input("give me a verb")
-noun = input("give me a noun")
-ing_verb = input("give me a verb that ends with ing")
-plural_noun3 = input("give me another plural noun")
-noun2 = input("give me a singular noun")
-plural_noun4 = input("give me a plural noun")
+keep_running = 'yes' #created a variable for the user inputi
+whle input='yes': #as long as the user inputs 'yes', this program will keep running
+    plural_noun1 = input("give me a plural noun")
+    plural_noun2 = input("give me a plural noun")
+    verb = input("give me a verb")
+    noun = input("give me a noun")
+    ing_verb = input("give me a verb that ends with ing")
+    plural_noun3 = input("give me another plural noun")
+    noun2 = input("give me a singular noun")
+    plural_noun4 = input("give me a plural noun")
 
 print("when i go to the arcade with my" + plural_noun1 + "there are lots of games to play. I spend lots of time there with my firends. In the game x-men you can be different" + plural_noun2 + "the point of the game is to" + verb + "every robot. You also need to save people. Then you can go to the next level. In the game Star Wars you are Luke Skywalker and you try to destroy every" + noun + "In a car racing/motorcycle racing game you need to beat every computerized vehicle that you are" + ing_verb + "against. There are a whole lot of other cool games. When you play some games you win" + plural_noun3 + "for certain scores. Once you're done you can cash in your tickets to get a big" + noun2 + "You can save your" + plural_noun4 + "for another time. When I went to this arcade I didn't believe how much fun it would be. So far I have had alot of fun every time I've been to this great arcade!")
 
