559e09053b47d2d52703bc16e25524e9bbcb09ee
lexiconPDX <chaungemail@gmail.com>
2020-04-27 07:57:25 -0700

    lab 09 - thanks, Tal!

--- Code/Lexi/lab12-peaks-v1.py
+++ Code/Lexi/lab12-peaks-v1.py
@@ -3,4 +3,4 @@ data = [1 ,2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 data_index = len(data)
 
 for i in range(0, data_index)
-    print(f'Index: {i}', f'Value: {data[i]}')
+print(f'Index: {i}', f'Value: {data[i]}')
