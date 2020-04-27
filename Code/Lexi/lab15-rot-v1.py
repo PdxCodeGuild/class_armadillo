99924d88899acb266ca9886ab8e14bd37948502c
lexiconPDX <chaungemail@gmail.com>
2020-04-27 09:55:38 -0700

    lab 15

--- Code/Lexi/lab15-rot-v1.py
+++ Code/Lexi/lab15-rot-v1.py
@@ -20,24 +20,4 @@ def rot13(text): # define the function
 
 user_input = input("What is the string? : ")
 # INTENDED RESULTprint(rot13('hello')) # uryyb
-print(f'New index of your rotated string is: {rot13(user_input)}')
-
-#for char in range NOT feasible
-# use for char in text
-# || for i in range(#)
-
-# we want to make f(x) self-contained - we only want to operate 
-# on parameters given, not on ones ousside the f(x)
-
-# lines 19 and 21 should remain ousside b/c they shouldn't be
-# used within the f(x)
-
-#side effects, limits how reusable f(x) is if ousside
-
-# input is shadowed beginning/end i.e. global vs local vars
-
-# BELOW IS EXAMPLE OF HOW TO CALL A FUNCTION IN A PRINT LN
-# def rotn(text, n):
-#   text = input('what is your text?')
-#   n = int(input('what is the roation amount?'))
-# print(rotn(text, n))
+print((rot13(user_input))
