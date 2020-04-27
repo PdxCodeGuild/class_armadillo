fccb9c04810737b309c8feb3326cee50c36f4553
lexiconPDX <chaungemail@gmail.com>
2020-04-27 08:44:51 -0700

    /usr/local/bin/python3

--- Code/Lexi/lab12-peaks-v1.py
+++ Code/Lexi/lab12-peaks-v1.py
@@ -5,15 +5,15 @@ data = [1 ,2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 # make sequences - lists are easiest to change
 peaks = []
-valleys = []
+valleys = []      # Sam helped me with lines 14, 16 - list wasn't populating
 peaks_n_valleys = []
 
 # go through each of the above sequences
 for i in range(1, len(data)-1):           #for loop (begin, end)
   if data[i] > data[i-1] and data[i] > data[i+1]:
-    peaks.append        # adding to peaks set
-  if data[i] < data[i-1] and data[i] < data[i-1]:
-    valleys.append      # adding to valley set
+    peaks.append(i)        # adding to peaks set
+  if data[i] < data[i+1] and data[i] < data[i-1]:
+    valleys.append(i)      # adding to valley set
 
 # add them into line 9's set
 peaks_n_valleys = peaks + valleys
@@ -30,6 +30,7 @@ for i in range(max(data), min(data), -1):
   print()
 
 # need to print out the above for loop in line 22
+
 print(f'The data is {data}')
 print(f'The peaks are: {peaks}')
 # only this print statement is working
