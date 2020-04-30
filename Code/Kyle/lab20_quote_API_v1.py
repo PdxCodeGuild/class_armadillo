6d9efb10151627de2effb4e58a2ee930aee569c8
kyle-harasimowicz <kyle.harasimowicz@gmail.com>
2020-04-29 18:08:12 -0700

    Captain's log: Day 25. Added regex functionality for user responses, 'strip()' and 'replace()' modules magically stopped working and I don't know why. All hope lost.

--- Code/Kyle/lab20_quote_API_v1.py
+++ Code/Kyle/lab20_quote_API_v1.py
@@ -5,9 +5,12 @@ import string
 import random
 import requests
 import json
+import re
+punctuation = string.punctuation
+print(punctuation)
 
-affirmatives = ['yes', 'y', 'sure', 'okay', 'fine', 'why not?']
-negatives = ['no', 'n', 'nope', 'negative', 'definitely not', 'no way']
+affirmatives = ['yes', 'y', 'sure', 'okay', 'fine', 'whynot', "definitely", "absolutely", "ofcourse"]
+negatives = ['no', 'n', 'nope', 'negative', 'definitelynot', 'noway', "absolutelynot", "ofcoursenot", "idontthinkso"]
 
 def generate_quote():
     response = requests.get("https://favqs.com/api/qotd")
@@ -19,7 +22,11 @@ def generate_quote():
 
 def another_quote():
     while True:
-        question = input("Would you like see another quote? ")
+        again = input("Would you like see another quote? ").lower()
+        question = again
+        question = question.replace(punctuation, ' ')
+        question = question.split()
+        question = ''.join(question)
         if question in affirmatives:
             print("")
             generate_quote()
@@ -27,11 +34,30 @@ def another_quote():
             question = question.title()
             print(f"{question}? Alight then - Goodbye!")
             quit()
+        elif question not in affirmatives and question not in negatives:
+            again = again.replace(punctuation, " ")
+            maybe_no = re.compile(' no ')
+            if maybe_no.search(again):
+                print(f"'{again}'? My best guess is that's probably a negative. Goodbye!")
+                quit()
+            else:
+                print("I'm sorry, I don't understand your response. Please answer affirmatively or negatively.")
+                
         else:
             print("I'm sorry, I don't understand your response. Please answer affirmatively or negatively.")
 
 
+# def maybe_a_no():
+#     test = input("Question:").lower()
+#     maybe_no = re.compile('no ')
+#     if maybe_no.search(test):
+#         print("User answer is probably a negative")
+#     else:
+#         print("I don't understand your answer")
+
+
 print("Welcome to Lab 20, version 1: Quote API")
-print("Here's a random quote from the wesbite, 'FaveQs.com:'\n ")
-generate_quote()
+print("Here's a random quote from the website, 'FaveQs.com:'\n ")
+#generate_quote()
 another_quote()
+#maybe_a_no()
