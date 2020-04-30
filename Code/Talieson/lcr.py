46e51e5369a12617c2f95dadae079f17cb8ee2cc
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-04-30 12:23:21 -0700

    functional version lcr.py

--- Code/Talieson/lcr.py
+++ Code/Talieson/lcr.py
@@ -22,39 +22,42 @@ while True:
     else:
         print("Not a valid entry.")
 
-# while True:
-# each player then takes turns rolling the dice
-for i in range(len(players)):
-    # if the player has less than 3 chips, they roll the same number of dice they have chips
-    # roll 3 dice
-    if players[i]["chips"] >= 3:
-        num_dice = 3
-    # roll number of dice
-    elif players[i]["chips"] == 2 or players[i]["chips"] == 1:
-        num_dice = players[i]["chips"]
-    else:
-        # don't roll any dice
-        num_dice = 0
-    print(f'{players[i]["name"]} rolled {num_dice} dice.')
-    for die_i in range(num_dice):
-        result = random.choice(dice_results)
-        print(result)
-        # if the die is L, move a chip to the player on the left (i-1)
-        if result == "Left":
-            players[i]["chips"] -= 1
-            players[i-1]["chips"] += 1
-        # if the die is R, move a chip to the player on the right (i+1)
-        elif result == "Right":
-            players[i]["chips"] -= 1
-            if i == len(players)-1:
-                players[0]["chips"] += 1
-            else:
-                players[i+1]["chips"] += 1
-        # if the die is C, move a chip to the center pot
-        elif result == "Center":
-            players[i]["chips"] -= 1
-            center += 1
-    print_players(players)
-
-
-# when only one player has chips, they won, ask the user if they want to play again
+winner = False
+while not winner:
+    # each player then takes turns rolling the dice
+    for i in range(len(players)):
+        if players[i]["chips"] + center == len(players) * 3:
+            print(f"{players[i]['name']} is the winner! They won {center} chips!")
+            winner = True
+            break
+        # if the player has less than 3 chips, they roll the same number of dice they have chips
+        # roll 3 dice
+        if players[i]["chips"] >= 3:
+            num_dice = 3
+        # roll number of dice
+        elif players[i]["chips"] == 2 or players[i]["chips"] == 1:
+            num_dice = players[i]["chips"]
+        else:
+            # don't roll any dice
+            num_dice = 0
+        if num_dice != 0:
+            print(f'{players[i]["name"]} rolled {num_dice} dice.')
+        for die_i in range(num_dice):
+            result = random.choice(dice_results)
+            print(result)
+            # if the die is L, move a chip to the player on the left (i-1)
+            if result == "Left":
+                players[i]["chips"] -= 1
+                players[i-1]["chips"] += 1
+            # if the die is R, move a chip to the player on the right (i+1)
+            elif result == "Right":
+                players[i]["chips"] -= 1
+                if i == len(players)-1:
+                    players[0]["chips"] += 1
+                else:
+                    players[i+1]["chips"] += 1
+            # if the die is C, move a chip to the center pot
+            elif result == "Center":
+                players[i]["chips"] -= 1
+                center += 1
+        print_players(players)
