de3f8fc2562476516c8c26ffac25c155fb67c75f
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-04-30 17:24:52 -0700

    added lab 21 framework

---
+++ Code/Troy/lab21 num_to_phrase.py
@@ -0,0 +1,80 @@
+# Lab 21 Number to Phrase
+# Troy Fitzgerald
+
+'''
+Convert a given number into its English representation. For 
+example: 67 becomes 'sixty-seven'. Handle numbers from 0-99.
+
+Hint: you can use modulus to extract the ones and tens digit.
+
+x = 67
+tens_digit = x//10
+ones_digit = x%10
+
+Hint 2: use the digit as an index for a list of strings.'''
+
+# import the modules.
+import math
+import string
+import random
+
+# assigns numbers in dictionary for 1-10.
+nums_1 = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten'
+}
+# assigns numbers in dictionary 11-19.
+nums_2 = {
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen'
+}
+# assigns numbers in dictionary for higher for numbers by 10.
+nums_3 = {
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety'
+} 
+
+# asks the user for a number.
+user_question = int(input('What is your number? '))
+
+# defines the function for the user input.
+def nums_to_phrase(nums):
+    # if number less than 10.
+    if nums < 10:
+        return nums_1[nums]
+    # if number less than 20
+    elif nums < 20:
+        return nums_2[nums]
+    # if number greater than 20 but less than 100.
+    elif nums 
+    
+nums = user_question
+print(nums_to_phrase(nums))
+
+    
+        
+
+        
+    
