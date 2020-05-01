f453aa4800fcd642bce78d42effa6a1264106906
lexiconPDX <chaungemail@gmail.com>
2020-05-01 14:28:53 -0700

    lab 22-rain-jai-makes-it-rain

--- Code/Lexi/lab21-nums-v1.py
+++ Code/Lexi/lab21-nums-v1.py
@@ -36,17 +36,19 @@ hundreds = {1: "one hundred and", 2: "two hundred and", 3: "three hundred and",
 # main run loop
 while True:
   # test input
-  number = (input("Enter a numeric value 0 - 999: "))
-  if number.isdigit():
-    number = int(number)
-    break
-  elif number in ['done']:
-    print("Thanks for using this code")
+
+
+  # if number.isdigit():
+  #   number = int(number)
+  #   break < - Jon said this is my problem
+  # # elif number in ['done']:
+  #   print("Thanks for using this code")
   
+  number = int(input("Enter a numeric value 0 - 999: "))
   # floor divide hundreds
   hundreds_place = number // 100
   # floor divide minus first index/hundreds' place to get tens' place
-  tens_place = (number - hundreds)// 10
+  tens_place = (number - hundreds*100)// 10
   # use modulo to get ones' place
   ones_place = number % 10
   # check if we have a remainder in the tens' place
@@ -59,3 +61,22 @@ while True:
     result = f'{hundreds[hundreds_place]} {tens_dict[tens_place]} {ones[ones_place]}'
   
   print(result)
+
+  '''So if you print out number anywhere after line 40-44 is doesn't do anything.
+
+If you print right after 39
+it does work
+
+if you comment line 40-44 it then prints out everything
+
+I would ask for input at the end of the result and use line 43-44 to ask whether or not they want to continue with the loop, otherwise break
+
+Oh also on line 42 you added break
+
+That breaks out of the while loop and won't print anything. I think that's the main culprit
+
+breaks will break which ever loop it is currently in
+
+line 40-41 are not needed because you can simply wrap int() around the input
+
+It will always be a number'''
