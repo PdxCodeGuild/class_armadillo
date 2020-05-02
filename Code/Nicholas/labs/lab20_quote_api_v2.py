a1044089aaa82236c835e57a1b72dd3c57f1fcad
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-05-01 23:47:10 -0600

    added lab 20 v2 in pro

--- Code/Nicholas/labs/lab20_quote_api_v2.py
+++ Code/Nicholas/labs/lab20_quote_api_v2.py
@@ -1,16 +1,16 @@
 import requests
 import json
 
-url = 'https://favqs.com/api/quotes?page=<page>&filter=<keyword>'
+
+page = 1
+keyword = input('enter a keyword to search for quotes: ') 
+url = f'https://favqs.com/api/quotes?page={page}&filter={keyword}'
 headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
 response = requests.get(url, headers=headers)
-
-# data = json.loads(response.text)
-# print(data)
+data = json.loads(response.text)
 
 
-keyword = input('enter a keyword to search for quotes: ') 
-url = f'https://favqs.com/api/quotes?page=<page>&filter={keyword}'
-response = requests.get(url)
-data = response.json()
-print(data)
+for quotes in data['quotes']: 
+    print({quotes['body']})
+    print({quotes['author']})
+
