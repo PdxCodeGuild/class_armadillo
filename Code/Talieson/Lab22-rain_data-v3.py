4938cb8d9b62251f6243fd761b8b94c14df6f257
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-05-04 14:42:14 -0700

    Updated Lab22-rain_data-v3.py tested, linted, commented, functioning.

--- Code/Talieson/Lab22-rain_data-v3.py
+++ Code/Talieson/Lab22-rain_data-v3.py
@@ -8,8 +8,6 @@ from collections import OrderedDict
 text_path = "C:/Users/talie/class_armadillo/code/talieson/data/rain_data.txt"
 with open(text_path, "r") as file:
     text = file.read()
-location_pattern = (r'\A.*')
-location = findall(location_pattern, text)
 
 
 # Use regular expression to identify the date and total amount of rain fall
@@ -24,30 +22,34 @@ def rain_by_data(data):
     # make our data into a list for the pyplot function
     x = list(data.keys())
     y = list(data.values())
-    # label our graph and axis
-    plt.xlabel("Date")
-    plt.ylabel("Rainfall (in 100ths of an inch)")
-    plt.title(f"Rain fall per day at {location}")
-    plt.plot(x, y)
-    plt.show()
+    # call our function to creat the graph
+    gen_graph(x, y)
 
 
-def rain_by_year(data, year):
-    year_dict = OrderedDict()
-    for key, value in data:
-        if year in key:
-            year_dict.update(key, value)
-    x = list(year_dict.keys())
-    y = list(year_dict.values())
-    # label our graph and axis
-    plt.xlabel(f"Date in {year}")
+def rain_by_date(data, date):
+    # dictionary with just the values we want
+    date_dict = {}
+    for key, value in data.items():
+        if date in key:
+            date_dict.update({key: value})
+    x = list(date_dict.keys())
+    y = list(date_dict.values())
+    # call our function to creat the graph
+    gen_graph(x, y, date)
+
+
+# takes x, y from other functions, styles graph
+def gen_graph(x, y, date="Recorded History"):
+    plt.xlabel(f"Date in {date}")
     plt.ylabel("Rainfall (in 100ths of an inch)")
     plt.title(f"Rain fall per day at {location}")
-    plt.plot(x, y)
+    plt.plot(x, y, "-bo", markevery=None)
     plt.show()
 
 
 dates = find_dates(text)
+location_pattern = (r'\A.*')
+location = findall(location_pattern, text)
 
 # create an empty dictionary to hold our data
 data = OrderedDict()
@@ -62,11 +64,30 @@ for date in dates:
     data.move_to_end(date[0], last=False)
 
 while True:
-    year = input("Which year do you want rain data from? ")
-    if year.isdigit():
-        year = int(year)
+    operation = input('''Enter the number for the operation you would you like to peform:
+        1: rainfall by date for all recorded history.
+        2: rainfall by month or year.
+        3: rainfall by month of a specific year.
+        E: Exit.
+            > ''')
+    if operation == '1':
+        rain_by_data(data)
+    elif operation == '2':
+        year = input('''
+Enter 4 digit year or uppercase 3 letter appreviation:
+    > ''')
+        rain_by_date(data, year)
+    elif operation == '3':
+        year_month = input('''
+enter in uppercase 3 letter appreviation - year. ex: <MAY-2020>
+    > ''')
+        rain_by_date(data, year_month)
+    elif operation == 'E':
+        exit()
     else:
-        print("please enter a valid year in digits. ex: <XXXX>")
+        print('''
+        ERROR: Not a valid input!
+        ''')
 
-    # Call our function
-    rain_by_year(data, year)
+
+# Call our function
