5bba342cdd215ae274c350d5b71496fff5f93588
Charlie Villanueva <infidel12@hotmail.com>
2020-05-04 14:59:36 -0700

    still working on version 2 lab 22

--- Code/Charlie/lab22_rain_data.py
+++ Code/Charlie/lab22_rain_data.py
@@ -1,45 +1,50 @@
+
+
+
+
 import requests
 from datetime import datetime
 import re
-import json
+import math
+
+
 # date = datetime.datetime.strptime('25-MAR-2016', '%d-%b-%Y')
 
+url = requests.get('https://or.water.usgs.gov/non-usgs/bes/multnomah.rain')
+
+multnomah_rain = url.text
+# get the rain-fall
 
 
-url = 'https://or.water.usgs.gov/non-usgs/bes/multnomah.rain'
-
-response = requests.get(url)
-
-multnomah_rain = json.loads(response.text)
 
 # Use regular expression to identify the date and total amount of rain fall
+
 def day_in_month(multnomah_rain):
-    pattern = (r'\d+-\w+-\d+\s+(\d+)', multnomah_rain)
-    monthly_rain = findall(pattern, multnomah_rain)
-    return monthly_rain
- 
 
+    # get the date
+    # listy = []
+    # get just the total rainfalls
+    monthly_rain = re.findall(r'\d+-\w+-\d+\s+(\d+)', multnomah_rain)
+    # get just the dates
+    monthly_date = re.findall(r'\d+-\w+-\d+', multnomah_rain)
+    # iterated over the rainfalls and joined them into a list of tuples.
+    for i in range(len(monthly_rain)):
+        # date = datetime.strptime(monthly_date[i], '%d-%b-%Y')
+        rain_fall_amount = int(monthly_rain[i])
+        # print(date)
+        tuples = (monthly_date[i],rain_fall_amount)
+        # tuples = listy.append(tuples)
+        total_rain_fall = 0
+        for i in rain_fall_amount:
+            total_rain_fall += rain_fall_amount[i]
+            total_rain_fall/rain_fall_amount = wow
+            print(wow)
 
- # call our function
-day_in_month = day_in_month(multnomah_rain
-# create an empty dictionary to hold our data
-daa = {}
-# iterate over the touples in dates
-for date in day_in_month:
-    # make the touple a list
-    day_in_month = list(day_in_month)
+        print(tuples)
+        # print(listy)
     
-    day_in_month[0] = datetime.datetime.strptime(day_in_month[0], '%d-%b-%Y')
-    # make our rainfall value into an integer
-    day_in_month[1] = int(day_in_month[1])
-    # add to dictionary with date as a key, and the integer as value
-    data.update({day_in_month[0]: day_in_month[1]})
+    # call our function
+day_in_month(multnomah_rain)
 
 
 
-
-# print(date.year)   # 2016
-# print(date.month)  # 3
-# print(date.day)    # 25
-# print(date)  # 2016-03-25 00:00:00
-# print(date.strftime('%d-%b-%Y'))  # 25-Mar-2016
