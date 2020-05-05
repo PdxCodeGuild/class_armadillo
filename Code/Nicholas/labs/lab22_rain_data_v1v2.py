599f5deda93902c034e5c9026a624b3bd1001a54
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-05-05 13:16:52 -0500

    added comments to lab 22

--- Code/Nicholas/labs/lab22_rain_data_v1v2.py
+++ Code/Nicholas/labs/lab22_rain_data_v1v2.py
@@ -9,6 +9,7 @@
 # Each tip is 0.01 inches of rainfall.
 #  [-, missing data]
 # Dates and times are PACIFIC STANDARD TIME.
+
 from datetime import datetime # import datetime
 import re # import regular expressions
 import requests
@@ -21,13 +22,14 @@ text = response.text
 rain_data = re.findall(r"(\d+\D\w+\D\d+)\s+(\d+)", text)  # use regular expressions to parse out the req'd first two columns
 # print(rain_data)
 for i in range(len(rain_data)):  #iterates over generated list of rain data
-    date = (rain_data[i][0])  #assigns first index as date 
+    date = rain_data[i][0]  #assigns first index as date 
     daily_rain = rain_data[i][1].replace('-','')  #assigns second index as rain. Replace blank rain data(-) with blank space. 
     date = datetime.strptime(date, '%d-%b-%Y')  # uses datetime.strptime to make each part of date accessible
     rain_data[i] = (date, daily_rain)  #makes date and daily rain into tuple
     # print(rain_data[i]) #prints tuples
 
 # version 2
+#mean
 total = 0 # start with zero total 
 for i in range(len(daily_rain)): #iterate over daily rain list
     total += int(rain_data[i][1])  #add all daily rain totals(convert list to int)
@@ -35,18 +37,20 @@ for i in range(len(daily_rain)): #iterate over daily rain list
 print(f'The mean rainfall is {mean} tips.')    
   
 
-  
+#variance  
 total = 0
 for i in range(len(daily_rain)):  # iterate over daily rain totals
     total += (int(rain_data[i][1]) - mean) ** 2  #add all daily rain totals, subtract mean, square
     variance = total / len(daily_rain)  #totals/days
 print(f'The variance is {variance}.')    
 
+#sd
 standard_deviation = math.sqrt(variance) #sd = variance squared
 print(f'The standard deviation is {standard_deviation}.')
 
-highest_rain = 0
-for i in range(int(daily_rain)): 
-    if (int(daily_rain)) > highest_rain:
+#highest day
+highest_rain = 0 # set highest rain var to 0
+for i in range(int(daily_rain)): #iterate over daily rain, convert list to int
+    if (int(daily_rain)) > highest_rain: # generate highest value
         highest_rain = int(daily_rain)
-print(f'The day with the most rain was {rain_data[i][0]} with {highest_rain} tips')    
+print(f'The day with the most rain was {rain_data[i][0]} with {highest_rain} tips')  # formatted string prints date and highest rain  
