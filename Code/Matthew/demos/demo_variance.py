2fcc35eb460ba222a93fc5a2b5f2c06b90178be0
Matthew Cooper <flux2341@gmail.com>
2020-05-05 09:36:31 -0700

    added labs 23 and 24 and solutions to 22

---
+++ Code/Matthew/demos/demo_variance.py
@@ -0,0 +1,28 @@
+
+
+import random
+import math
+
+# https://en.wikipedia.org/wiki/Arithmetic_mean
+def mean(nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total / len(nums)
+
+# https://en.wikipedia.org/wiki/Variance
+def variance(nums):
+    average = mean(nums)
+    total = 0
+    for num in nums:
+        total += (num - average) ** 2
+    return total / len(nums)
+
+# https://en.wikipedia.org/wiki/Standard_deviation
+def standard_deviation(nums):
+    return math.sqrt(variance(nums))
+
+nums = [random.randint(1, 99) for _ in range(100)]
+print(f'The mean is {mean(nums)}')
+print(f'The variance is {variance(nums)}')
+print(f'The standard deviation is {standard_deviation(nums)}')
