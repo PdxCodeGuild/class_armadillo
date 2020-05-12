4a82626d79d01cd53056558b7b1e74ff4bdff744
lexiconPDX <chaungemail@gmail.com>
2020-05-12 11:00:06 -0700

    madlib

--- Code/Lexi/Flask/demo1/app.py/demo2/templates/app.py
+++ Code/Lexi/Flask/demo1/app.py/demo2/templates/app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    
+
+   animal = 'armadillo'
+   texture = 'scales'
+   noun = 'dirt'
+   madlib = f'Suzy had a {animal} its {texture} was white as {noun}'
    # passing KWARGS below
-   return render_template('index.html', result="testing result")
+   return render_template('index.html', result=madlib)
 
