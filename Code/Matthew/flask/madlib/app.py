8d7330d6ad30f270d3b5f00bca1aa38c3b7c7103
Matthew Cooper <flux2341@gmail.com>
2020-05-12 11:57:49 -0700

    updated madlib demo

--- Code/Matthew/flask/madlib/app.py
+++ Code/Matthew/flask/madlib/app.py
@@ -1,17 +1,30 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def index(): # view
 
-    animal = 'armadillo'
-    texture = 'scales'
-    noun = 'dirt'
-    madlib = f"Suzy had a little {animal}, its {texture} was white as {noun}"
+    print(request.form)
+    message = ''
+    if request.method == 'POST': # we received a form submission
 
-    return render_template('index.html', result=madlib)
+        # the keys of the dictionary request.form
+        # are the *name* attributes of the input fields inside the form
+        animal = request.form['animal']
+        texture = request.form['texture']
+        noun = request.form['noun']
+        if noun.isdigit(): # if the user entered a number for the noun
+            message = 'noun cannot be a number'
+            madlib = ''
+        else:
+            madlib = f"Suzy had a little {animal}, its {texture} was white as {noun}"
+    else: # we received a get request
+        madlib = ''
+    animals = ['armadillo', 'sloth', 'baby elephant', 'honeybadger', 'llama', 'raven']
+    animals.sort()
+    return render_template('index.html', result=madlib, animals=animals, message=message)
 
 
 
