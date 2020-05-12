24a69155d9d0e132850e9f7197de9185eca99591
lexiconPDX <chaungemail@gmail.com>
2020-05-12 10:01:46 -0700

    render template

--- Code/Lexi/Flask/demo1/app.py/app.py
+++ Code/Lexi/Flask/demo1/app.py/app.py
@@ -1,18 +1,20 @@
-from flask import flask
-app = Flask(_name_)
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route('/') # decorator
 def index():    # local home directory is typically 'index'
-    html = '<html><head></head></body>'
-    html += '<ul>'
-    for i in range(100):
-        html += f'<li>{i}</li>'
-        html+= '</ul>'
-        html += '</body></html>'
-        print(html)
+    # html = '<html><head></head></body>'
+    # html += '<ul>'
+    # for i in range(100):
+    #     html += f'<li>{i}</li>'
+    #     html+= '</ul>'
+    #     html += '</body></html>'
+    #     print(html)
+    #     return html
 
-        return html
+name = 'bob'
+return render_template('index.html', name='bob')
 
-@app.route('/about')
+@app.route('/about') # this is called a 'view'
 def about():
     return 'Welcome to the about page'
