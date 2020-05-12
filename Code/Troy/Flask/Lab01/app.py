de33d8904b1d9e333fa3041c1d5b4c63869f9b6a
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-05-12 15:22:53 -0700

    updated flask lab 01

--- Code/Troy/Flask/Lab01/app.py
+++ Code/Troy/Flask/Lab01/app.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+import string
+import random
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+    # print('Please make a password.  It should be 10 characters long. ')
+
+    # input('I will make a password using random choices. Press enter to get your password... ')
+
+    # for i in range(10):
+    #     print(random.choice(alphabet), end= '')
+
+    print(request.form)
+    message = ''
+
+    return render_template('index.html')
