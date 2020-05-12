bfe7ed9d8e979003150dff8869afea8324effd94
lexiconPDX <chaungemail@gmail.com>
2020-05-12 09:53:59 -0700

    ul add

--- Code/Lexi/Flask/demo1/app.py/app.py
+++ Code/Lexi/Flask/demo1/app.py/app.py
@@ -3,8 +3,14 @@ app = Flask(_name_)
 
 @app.route('/') # decorator
 def index():    # local home directory is typically 'index'
-    return 'Hello World!'
+    html = <html><head><head><body>'
+    html += '<ul>'
+    for i in range(100):
+        html += f'<li>{i}</li>'
+        html+= '</ul>'
+        html += '</body></html>'
+        print(html)
 
-@app.route('/')
+@app.route('/about')
 def about():
-    return 'Hello World!'
+    return 'Welcome to the about page'
