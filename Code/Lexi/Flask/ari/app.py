0d3897e7d8e9a1e8913289d20379b2e7c939094c
lexiconPDX <chaungemail@gmail.com>
2020-05-13 10:13:05 -0700

    adding ari

--- Code/Lexi/Flask/ari/app.py
+++ Code/Lexi/Flask/ari/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index(): #view
+    return render_template('index.html', message1='hello', message2='ahoy')
 
-    @app.route('/about/')
-def llama():
-    return 'You are a llama'
+@app.route('/about/')
+def about():
+    return render_template('about.html')
