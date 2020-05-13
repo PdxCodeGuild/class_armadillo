b3c33734b28e57b449daff27e6470748ac4ca370
dirtTastesGood <keegood8@gmail.com>
2020-05-13 10:40:43 -0700

    add flask lab2

---
+++ Code/Keegan/labs/flask/lab1/rot13/py/rot13.py
@@ -0,0 +1,23 @@
+import string
+
+def get_rotated(char, char_set, r):
+    return char_set[(char_set.index(char) + r) % len(char_set)]
+
+def rotate_string(user_string, r_factor):
+    r_factor = int(r_factor) % 26
+    alphabet       = string.ascii_lowercase
+    alphabet_up    = string.ascii_uppercase
+    numbers        = string.digits
+
+    rotated_string = ''
+    for char in user_string:
+        if char in alphabet:
+            rotated_string += get_rotated(char, alphabet, r_factor)
+        elif char in alphabet_up:
+            rotated_string += get_rotated(char, alphabet_up, r_factor)
+        elif char in numbers:
+            rotated_string += get_rotated(char, numbers, r_factor)
+        else:
+            rotated_string += char
+
+    return rotated_string
