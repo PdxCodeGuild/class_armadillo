d8243d7a575d65fa9f76c216c399027aee92e5c4
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-05-12 17:47:59 -0700

    updated flask lab 01

--- Code/Troy/Flask/Lab01/app.py
+++ Code/Troy/Flask/Lab01/app.py
@@ -8,11 +8,10 @@ import random
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    output = ""
     if request.method == "POST":
         alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
         password_length = int(request.form["password_length"])
-
-        output = ""
 
         for i in range(password_length):
             output += random.choice(alphabet)
