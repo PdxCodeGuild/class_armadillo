3ccbc1b72a92ab7f4647b3bb86af56d187f70c04
lexiconPDX <chaungemail@gmail.com>
2020-05-13 10:20:38 -0700

    reviewing how to do request.form

--- Code/Lexi/Flask/ari/app.py
+++ Code/Lexi/Flask/ari/app.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(): #view
+    if request.method == 'POST':
+        print('index view - post')
+    else:
+        print('index view -get')
+
     return render_template('index.html', message1='hello', message2='ahoy')
 
 @app.route('/about/')
