5fdae9986127b28c37403a4efb453bb4d2509bfa
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-05-13 13:46:19 -0500

    added flask lab01 simple

--- Code/Nicholas/Flask/lab01/app.py
+++ Code/Nicholas/Flask/lab01/app.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/", methods=['POST', 'GET'])
 def index():
-   
+    units_dict = {
+    "ft" : 0.3048,
+    "mi" :1609.34,
+    "m" : 1,            #various unit's equivalent to 1 meter
+    "km" : 1000,
+    "yd" : 0.9144,
+    "in" : 0.0254
+    }
     print(request.form)
     if request.method == 'POST':
-        user_input = request.form[float(input("Enter a length in feet to convert to meters: "))]
-        meters = feet*0.3048  #feet to meters conversion  
+        user_input = request.form['user_input']
+        units = request.form['units']
+        user_input = float(user_input)
+        conversion = round(user_input * units_dict[units],3)
+        result = f'{user_input} {units} equals {conversion}'
     else: 
-        meters = ''     
-    return render_template('index.html', result=meters)
+        print(request.form)    
+        result = ''
+    return render_template('index.html', units=units, result=result)
+
+
+
+
     
     
     
