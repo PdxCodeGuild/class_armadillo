c0f2b41ef4640e6026b86cdc551a518ef1a2e52d
lexiconPDX <chaungemail@gmail.com>
2020-05-13 10:21:55 -0700

    converting the immutable dict to a dictionary

--- Code/Lexi/Flask/ari/app.py
+++ Code/Lexi/Flask/ari/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index(): #view
-    if request.method == 'POST':
+    if request.method == 'POST': # form submission
         print('index view - post')
+        form_data = dict(request.form)
+        print(form_data)
     else:
         print('index view -get')
-
+        print(request.form) # empty - did not submit a form to get here
     return render_template('index.html', message1='hello', message2='ahoy')
 
 @app.route('/about/')
