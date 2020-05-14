11d34733e956594c17f67fd5944eee3ea5f2c40f
Lawrence <trehouse650@gmail.com>
2020-05-14 09:16:21 -0800

    Finished flask lab01 simple

--- Code/Lawrence_House/Flask Labs/Lab01/app.py
+++ Code/Lawrence_House/Flask Labs/Lab01/app.py
@@ -3,24 +3,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    print(request.form)
+    
+    output = ""
+    meter_output = ""
     if request.method == 'POST':
-        user_distance = request.form['user_distance']
         user_unit = request.form['user_unit']
-        print(user_distance, user_unit)
+        user_distance = int(request.form['user_distance'])
         if user_unit == "mi" or user_unit == "mile":
-            meter_output = (user_distance * 1609.34)
+            output = (user_distance * 1609.34)
         elif user_unit == 'ft' or user_unit == 'feet':
-            meter_output = (user_distance * 0.3048)
+            output = (user_distance * 0.3048)
         elif user_unit == 'km' or user_unit == 'kilometer':
-            meter_output = (user_distance * 1000)
+            output = (user_distance * 1000)
         elif user_unit == 'm' or user_unit == 'meter':
-            meter_output = (user_distance * 1)
+            output = (user_distance * 1)
         elif user_unit == 'yd' or user_unit == 'yard':
-            meter_output = (user_distance * 0.9144)
+            output = (user_distance * 0.9144)
         elif user_unit == 'in' or user_unit == 'inch':
-            meter_output = (user_distance * 0.0254)
+            output = (user_distance * 0.0254)
+    
 
-
-    return render_template('index.html', result=meter_output)
+    return render_template('index.html', result = output)
