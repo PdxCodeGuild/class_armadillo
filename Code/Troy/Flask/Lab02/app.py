8c7331904c64970ca67e6fbc98230b58e2192ee5
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-05-14 21:13:13 -0700

    updated flask  lab 02

--- Code/Troy/Flask/Lab02/app.py
+++ Code/Troy/Flask/Lab02/app.py
@@ -8,26 +8,37 @@ import json
 #defines variable app and assigns it from Flask.
 app = Flask("honey_do_list_app")
 
-
-
-chore = '{"chore": "mow_the_lawn"}'
-data = json.loads(database) # json string -> python dictionary
-print(data['chore']) # bob
-data = json.puts(database) # python dictionary -> json string
-print(database)
-
-def load_database():
+# defines the function for chores.
+def load_chores():
+    # opens the file.
     with open('database.json', 'r') as file:
-        data = json.loads(file.read())
-    return data
-# saving and loading the database
-def save_database(data):
-    with open('database.json', 'w') as file:
-        file.write(json.dumps(data))
+        # reads the text
+        text = file.read()
+    # turns the json string into a python dictionary.
+    return json.loads(text)
 
-@app.route('/', methods=['GET', 'POST'])
+# saving and loading the database
+def save_chores(data):
+    # opens the file to be written.
+    with open('database.json', 'w') as file:
+         # python dictionary -> json string
+        text = json.dumps(data, indent=4)
+        # writes the file.
+        file.write(text)
+        print(data)
+
+
+@app.route('/')
+
 def index():
     if request.method == 'POST':
         contact_name = request.form['input_text']
         print(contact_name)
     return render_template('index.html')
+    
+
+
+# chore = '{"chore": "mow_the_lawn"}'
+# data = json.loads(database) # json string -> python dictionary
+# print(data['chore']) # bob
+
