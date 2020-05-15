e46f8dece458bed9c497e19f831450bb69ef858f
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-05-14 22:38:03 -0700

    updated flask lab 02

--- Code/Troy/Flask/Lab02/app.py
+++ Code/Troy/Flask/Lab02/app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 import json
 
 #defines variable app and assigns it from Flask.
-app = Flask("honey_do_list_app")
+app = Flask(__name__)
 
 # defines the function for chores.
 def load_chores():
@@ -17,24 +17,30 @@ def load_chores():
     # turns the json string into a python dictionary.
     return json.loads(text)
 
-# saving and loading the database
+# saving and loading the database.
 def save_chores(data):
     # opens the file to be written.
     with open('database.json', 'w') as file:
-         # python dictionary -> json string
+        # python dictionary -> json string
         text = json.dumps(data, indent=4)
         # writes the file.
         file.write(text)
-        print(data)
+        # print(data)
 
-
-@app.route('/')
-
+# view is set to GET and POST.
+@app.route('/', methods=['GET', 'POST'])
+# assigns the function.
 def index():
-    if request.method == 'POST':
-        contact_name = request.form['input_text']
-        print(contact_name)
-    return render_template('index.html')
+    # assigns variable to the function. 
+    data = load_chores()
+    # renders the template and runs the html file and accesses the value at the specified objects json key.
+    return render_template('index.html', honey_do_list=data['honey_do_list'])    
+
+
+    # if request.method == 'POST':
+    #     contact_name = request.form['input_text']
+    #     print(contact_name)
+    # return render_template('index.html')
     
 
 
