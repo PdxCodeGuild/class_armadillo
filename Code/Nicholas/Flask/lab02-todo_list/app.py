1d473d31db4ef84203e95f73c4f8adeb15e3f443
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-05-14 22:03:34 -0500

    Flask lab02, light progress

--- Code/Nicholas/Flask/lab02-todo_list/app.py
+++ Code/Nicholas/Flask/lab02-todo_list/app.py
@@ -1,20 +1,31 @@
 import json
 from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
 def load_database():
     with open('database.json', 'r') as file:
         data = json.loads(file.read())
     return data
 
-def save_database(todos):
+def save_database(data):
     with open('database.json', 'w') as file:
-        file.write(json.dumps(data))    
+        file.write(json.dumps(data, indent=4))    
 
-app = Flask(__name__)
-@app.route('/', method = ['GET','POST'])
-
+@app.route('/', methods = ['GET','POST'])
 def index():
+    text= ''
+    priority = ''
+    result= ''
     data = load_database()
+    if request.method == "POST": 
+        task = request.form['task']
+        priority = request.form['priority']
+        todos = {'text': task,
+                 'priority': priority}
+        data["todos"].append(todos)        
+        save_database(data) 
+        result = data["todos"]
+        
+    return render_template('index.html', task=text, priority=priority, result=result)
 
-    return render_template('index.html', todos = data["todos"])
