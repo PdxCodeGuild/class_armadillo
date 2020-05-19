7599a129652dc261ea6215daacb4f171533fa595
Adam Meier <meieradamn@gmail.com>
2020-05-18 18:13:59 -0700

    Lab updates

---
+++ Code/Adam/Flask/lab02/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+import json
+
+def load_database():
+    with open('db.json', 'r') as file:
+        text = file.read()
+    return json.loads(text)
+
+
+def save_database(data):
+    with open('db.json', 'r') as file:
+        text = json.dumps(data, indent=4)
+        file.write(text)
+
