975219dfcdef9959ea3c9adc857ba45b1ae8d24f
seanyboy1 <infidel12@hotmail.com>
2020-05-19 23:55:43 -0700

    update

--- Code/Charlie/django/mysite/polls/models.py
+++ Code/Charlie/django/mysite/polls/models.py
@@ -1,13 +1,15 @@
 import datetime
-from django.utils import timezone
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >=  timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- Code/Charlie/django/mysite/polls/views.py
+++ Code/Charlie/django/mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Choice, Question
 
 
@@ -10,11 +11,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
