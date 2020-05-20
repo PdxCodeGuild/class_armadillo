6fd1d1139dcbb8fd43cd3c761ebad4587d898ddf
lexiconPDX <chaungemail@gmail.com>
2020-05-19 17:53:36 -0700

    - Create model Question exit() exit() quit clr

--- Code/Lexi/django/mysite/mysite/settings.py
+++ Code/Lexi/django/mysite/mysite/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
--- Code/Lexi/django/mysite/polls/models.py
+++ Code/Lexi/django/mysite/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# equivalent to:
+# from django.db.models import Model
 
-# Create your models here.
+# this question class inherits everything this model does - class is Uppercase
+class Question(models.Model): # CharField is also a class name
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
