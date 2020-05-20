3ed2de5e82916e29aef17c8243fda4b088786698
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-05-20 14:15:04 -0700

    modified django part 3

--- Code/Troy/Django/mysite/polls/views.py
+++ Code/Troy/Django/mysite/polls/views.py
@@ -11,8 +11,8 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You\'re voting on question %s. % question_id"
-    )
+    return HttpResponse("You\'re voting on question %s. % question_id")
+    
 
     
 
