8467dcfe8639edb3af482a2dbc4b7c3c12e4758f
vladimirantigua <vladimirantigua@gmail.com>
2020-05-20 16:13:15 -0700

    lab01 polls adding more Writing more views

--- Code/vlad/django_folder/mysite/polls/admin.py
+++ Code/vlad/django_folder/mysite/polls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+admin.site.register(Question) # this will add question to the admin panel 
+admin.site.register(Choice) # this will add Choice to the admin panel 
