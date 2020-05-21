f5e084a26d4a1bd980492d7b37e68e3617c6bcc1
kyle-harasimowicz <kyle.harasimowicz@gmail.com>
2020-05-21 07:55:22 -0700

    updated directory

--- Code/Kyle/django/lab01/mysite/mysite/urls.py
+++ Code/Kyle/django/lab01/mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""blogproj URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('blogapp/', include('blogapp.urls'))
 ]
