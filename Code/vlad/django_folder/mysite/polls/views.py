e6a7273cf69db2b258efc29cb179cd5b3d590769
vladimirantigua <vladimirantigua@gmail.com>
2020-05-20 17:44:43 -0700

    added Lib1 Polls A shortcut: get_object_or_404()

--- Code/vlad/django_folder/mysite/polls/views.py
+++ Code/vlad/django_folder/mysite/polls/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
 
 from .models import Question
 
 
 
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
