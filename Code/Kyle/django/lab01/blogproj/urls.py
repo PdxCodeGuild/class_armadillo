bc0e42d1c7031c460e8c1ce0f869bac202d7d6a9
kyle-harasimowicz <kyle.harasimowicz@gmail.com>
2020-05-20 22:22:27 -0700

    cloned repo after system crash

--- Code/Kyle/django/lab01/blogproj/urls.py
+++ Code/Kyle/django/lab01/blogproj/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+<!-- test code -->
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogapp/', include('blogapp.urls'))
