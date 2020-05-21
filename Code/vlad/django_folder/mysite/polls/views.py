b68dfa7f49af1af7bbba91d469dfb215faec07d4
vladimirantigua <vladimirantigua@gmail.com>
2020-05-20 17:10:51 -0700

    Lab01 Polls update index view in polls/views.py

--- Code/vlad/django_folder/mysite/polls/views.py
+++ Code/vlad/django_folder/mysite/polls/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
