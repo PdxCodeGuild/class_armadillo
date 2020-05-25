73b20344e161695bf0a6e23f8ff77e249c8d95c8
lexiconPDX <chaungemail@gmail.com>
2020-05-25 09:34:24 -0700

    lab 09 v4 resubmit

--- Code/Lexi/django/mysite/mysite/urls.py
+++ Code/Lexi/django/mysite/mysite/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
+#project URLs
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
--- Code/Lexi/django/mysite/polls/views.py
+++ Code/Lexi/django/mysite/polls/views.py
@@ -1,25 +1,28 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+from .models import Choice, Question
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
