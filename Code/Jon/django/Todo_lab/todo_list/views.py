0ddfc0f0412ff3abfce7c899d7c61c8fd9a4c68c
Jonathan Yates <carlock1609@gmail.com>
2020-05-26 16:11:13 -0700

    created and delete done

--- Code/Jon/django/Todo_lab/todo_list/views.py
+++ Code/Jon/django/Todo_lab/todo_list/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import TodoItem
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    
-    return render(request, 'todo_list/index.html')
+    chores = TodoItem.objects.all().order_by('created_date')
+
+    context = {
+        'chores': chores,
+    }
+
+    return render(request, 'todo_list/index.html', context)
+
+def delete(request):
+    chore_id = request.POST['delete']
+    chore = TodoItem.objects.get(id=chore_id)
+    chore.delete()
+
+    return HttpResponseRedirect(reverse('todo_list:index'))
+
+def create(request):
+    name = request.POST['name']
+    description = request.POST['description']
+    TodoItem.objects.create(name=name, description=description)
+
+    return HttpResponseRedirect(reverse('todo_list:index'))
