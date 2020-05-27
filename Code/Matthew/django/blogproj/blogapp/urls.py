b71d8b85fb8e1bac0923e4a544487a8b2678c38d
Matthew Cooper <flux2341@gmail.com>
2020-05-27 09:45:57 -0700

    updated blog app

--- Code/Matthew/django/blogproj/blogapp/urls.py
+++ Code/Matthew/django/blogproj/blogapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # localhost:8000/5/
     # localhost:8000/23/
     path('<int:blog_post_id>/', views.detail, name='detail'),
+    # localhost:8000/new/
     path('new/', views.create_post_page, name='create_post_page'),
+    # localhost:8000/save_post/
     path('save_post/', views.save_post, name='save_post'),
 ]
