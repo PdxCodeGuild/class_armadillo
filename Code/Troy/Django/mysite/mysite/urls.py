f38f26aa987e5e9f56a0d9fe7f7c30b9afd00e38
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-05-27 14:41:32 -0700

    added django lab 3 framework

--- Code/Troy/Django/mysite/mysite/urls.py
+++ Code/Troy/Django/mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
     
 ]
