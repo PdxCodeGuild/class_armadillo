fd25862f2cd701e4447d49a1eba64fe644409161
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-05-27 14:01:41 -0400

    added mob lab2 progress

--- Code/Nicholas/Django/lab02-todos/todoproj/todoapp/views.py
+++ Code/Nicholas/Django/lab02-todos/todoproj/todoapp/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Todos 
 
 def index(request):
     todos = Todos.objects.order_by('publish_date')
+    completed = Todos.objects.order_by('completed_date')
     context={
         'todos': todos
     }
     return render(request,'todoapp/index.html', context)
+
+def detail(request, todos_id):
+    todo = get_object_or_404(Todos, pk=todos_id)
+    completed = Todos.objects.order_by('completed_date')
+    return render(request,'todoapp/index.html', context)
+        
