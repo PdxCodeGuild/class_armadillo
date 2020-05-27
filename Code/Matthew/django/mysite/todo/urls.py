9143d91f51dbc0e0bf83a4c377082c7ef671051e
Matthew Cooper <flux2341@gmail.com>
2020-05-26 17:14:21 -0700

    more mob programming on the todo list

--- Code/Matthew/django/mysite/todo/urls.py
+++ Code/Matthew/django/mysite/todo/urls.py
@@ -5,5 +5,11 @@ from . import views
 
 app_name = 'todo'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # localhost:8000/todo/
+    path('', views.index, name='index'), 
+    # localhost:8000/todo/create/
+    path('create/', views.create, name='create'),
+    # localhost:8000/todo/complete/5/
+    # localhost:8000/todo/complete/89/
+    path('complete/<int:todo_item_id>/', views.complete, name='complete')
 ]
