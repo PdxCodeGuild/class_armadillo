2fd3a2b080fe4e86081f2c2b212d71eabe233957
Matthew Cooper <flux2341@gmail.com>
2020-05-27 10:59:47 -0700

    finished mob programming

--- Code/Matthew/django/mysite/todo/admin.py
+++ Code/Matthew/django/mysite/todo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import TodoItem
+from .models import TodoItem, Priority
 
 admin.site.register(TodoItem)
+admin.site.register(Priority)
+
--- Code/Matthew/django/mysite/todo/views.py
+++ Code/Matthew/django/mysite/todo/views.py
@@ -2,27 +2,30 @@ from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 
-from .models import TodoItem
+from .models import TodoItem, Priority
 
 def index(request):
     todo_items = TodoItem.objects.order_by('completed_date')
-    # print(todo_items)
+    priorities = Priority.objects.all()
     context =  {
         'title': 'Todo',
         'todo_items': todo_items,
+        'priorities': priorities,
     }
     return render(request, 'todo/index.html', context)
 
 
 def create(request):
-    # print(request.POST) # vertify we are receiving the form data
+    print(request.POST) # vertify we are receiving the form data
 
     # get todo text out of the form
     todo_text = request.POST['todo_text']
+    priority_id = request.POST['priority_id']
     # create an instance of our model
     todo_item = TodoItem(description=todo_text,
                             created_date=timezone.now(),
-                            completed_date=None)
+                            completed_date=None,
+                            priority_id=priority_id)
     # save the instance
     todo_item.save()
     # redirect to the index view 
