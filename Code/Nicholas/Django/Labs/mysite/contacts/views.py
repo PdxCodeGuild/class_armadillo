13b81cddb15c48cd6317b30f0436d7902d377159
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-05-27 19:28:57 -0400

    Django lab3 progress

---
+++ Code/Nicholas/Django/Labs/mysite/contacts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Contacts
+
+
+def index(request):
+    contacts = Contacts.objects.order_by('first_name')
+    context = {
+        'title': 'Contacts List',
+        'contacts': 'Contacts'
+    }
+    return render(request, 'contacts/index.html', context)
