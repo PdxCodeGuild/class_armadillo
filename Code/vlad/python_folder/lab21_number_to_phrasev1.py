5b72bfcc0d716d0e39a3708ac5bd4d87cb91e989
vladimirantigua <vladimirantigua@gmail.com>
2020-05-26 18:48:14 -0700

    Lab 21: Number to Phrase Version 1

--- Code/vlad/python_folder/lab21_number_to_phrasev1.py
+++ Code/vlad/python_folder/lab21_number_to_phrasev1.py
@@ -39,8 +39,11 @@ import string
 
 #def convert_num_to_word():
 
+# use if statements to check if number are in the 11
 
-single_digit = {  
+# to count for numbers zero to 10
+digit_one = { 
+    0: "zero", 
     1:'One', 
     2:'Two', 
     3:'Three', 
@@ -49,31 +52,102 @@ single_digit = {
     6:'Six',
     7:'Seven',
     8:'Eight',
-    9:'Nine'
+    9:'Nine',
+    10:'Ten'
 }
 
-two_digit = {
-    1:'Ten', 
-    2:'Twenty', 
-    3:'Thirty', 
-    4:'Forty', 
-    5:'Fifty', 
-    6:'Sixty',
-    7:'Seventy',
-    8:'Eighty',
-    9:'Ninety'
+# to count for the numbers in teen 
+digit_two = {
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen'
 }
 
-user_input = int(input('enter a number: '))
-x = user_input
-ones_digit  = x%10
-# print(single_digit)
-tens_digit  = x//10
-# print(tens_digit)
-# print(two_digit[60],single_digit[7])
-print((two_digit[tens_digit]) + '-' + (single_digit[ones_digit]))
+# count for numbers ten digits 
+digit_three = {
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety'
+} 
 
 
+user_input = int(input('enter a number to convert to words please: '))
+
+def numbers_phrase(nums):
+   
+    if nums < 10:
+        return digit_one[nums]
+
+    elif nums < 20:
+        return digit_two[nums]
+    
+    elif nums < 100:
+        tens_digit = nums//10
+        ones_digit = nums%10
+        return digit_three[tens_digit] + '-' + digit_one[ones_digit]
+        
+
+    
+nums = user_input
+phrase = (numbers_phrase(nums))
+print(f'your number to phase is: {phrase}')
+
+
+# def number(Number):
+    # if 0 <= Number <= 19:
+    #     return num2words1[Number]
+    # elif 20 <= Number <= 99:
+    #     tens, remainder = divmod(Number, 10)
+    #     return num2words2[tens - 2] + '-' + num2words1[remainder] if remainder else num2words2[tens - 2]
+    # else:
+    #     print('Number out of implemented range of numbers.')
+
+
+# single_digit = { 
+#     0: "zero", 
+#     1:'One', 
+#     2:'Two', 
+#     3:'Three', 
+#     4:'Four', 
+#     5:'Five', 
+#     6:'Six',
+#     7:'Seven',
+#     8:'Eight',
+#     9:'Nine',
+#     10:'Ten'
+# }
+
+# two_digit = {
+#     1:'Ten', 
+#     2:'Twenty', 
+#     3:'Thirty', 
+#     4:'Forty', 
+#     5:'Fifty', 
+#     6:'Sixty',
+#     7:'Seventy',
+#     8:'Eighty',
+#     9:'Ninety'
+# }
+
+# user_input = int(input('enter a number: '))
+# x = user_input
+# ones_digit  = x%10
+# # print(single_digit)
+# tens_digit  = x//10
+# # print(tens_digit)
+# # print(two_digit[60],single_digit[7])
+# print((two_digit[tens_digit]) + '-' + (single_digit[ones_digit]))
 
     # num = input('Which ')
     # if num == '0': 
@@ -96,3 +170,12 @@ print((two_digit[tens_digit]) + '-' + (single_digit[ones_digit]))
     #         print("Eight ", end = " ")
     # elif num == '9': 
     #         print("Nine ", end = " ")     
+
+    # def number(Number):
+    # if 0 <= Number <= 19:
+    #     return num2words1[Number]
+    # elif 20 <= Number <= 99:
+    #     tens, remainder = divmod(Number, 10)
+    #     return num2words2[tens - 2] + '-' + num2words1[remainder] if remainder else num2words2[tens - 2]
+    # else:
+    #     print('Number out of implemented range of numbers.')
