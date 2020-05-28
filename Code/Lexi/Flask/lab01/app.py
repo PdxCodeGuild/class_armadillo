b10d1932876068a61d764754b135e9d4e52f41c7
lexiconPDX <chaungemail@gmail.com>
2020-05-28 13:15:39 -0700

    Flask lab 01 changed logic

--- Code/Lexi/Flask/lab01/app.py
+++ Code/Lexi/Flask/lab01/app.py
@@ -2,22 +2,48 @@
 #   - Simple version: the user just enters in the number of characters in the password
 
 from flask import Flask, request, render_template
+from string import ascii_lowercase, punctuation, digits
 import string
 import random
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    output = ""
+
     if request.method == 'POST':
-        alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-        password_length = int(request.form["password_length"])
+        user_value = request.form['number']
+        i = 0
+        keep_it_going = True
+        possible_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+        password_length = int(user_value)
 
-        for i in range(password_length):
-            output += random.choice(alphabet)
+        rando = []
+        while i in range(password_length):
+            rando.append(random.choice(possible_chars))
+            i += 1
+            rando_string = ''.join(rando)    
+            output = rando_string
+     
+    else:
+        output = ''
 
-    print(request.form)
+    message = ''
+
+    return render_template('index.html', output=output)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     output = ""
+#     if request.method == 'POST':
+#         alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+#         password_length = int(request.form["password_length"])
+
+#         for i in range(password_length):
+#             output += random.choice(alphabet)
+
+#     print(request.form)
    
 
-    return render_template('index.html', password = output)
+#     return render_template('index.html', password = output)
