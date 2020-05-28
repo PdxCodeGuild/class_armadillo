f3593a1f970f2b87adf20931d6293d3f219b11ae
vladimirantigua <vladimirantigua@gmail.com>
2020-05-28 16:22:45 -0700

    Lab 3: Contacts aded Define Your Models

--- Code/vlad/django_folder/labs/contacts/views.py
+++ Code/vlad/django_folder/labs/contacts/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# def index(request):
+#   return HttpResponse('hello world!') # in order to see hello world I needed to use the following URL: http://localhost:8000/contacts/index/
+from django.shortcuts import render
+
 def index(request):
-  return HttpResponse('hello world!') # in order to see hello world I needed to use the following URL: http://localhost:8000/contacts/index/
+    context = {
+        'message': 'Hello World!!'
+    }
+    return render(request, 'contacts/index.html', context)
