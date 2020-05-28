ba0d08c6c74e270bad2d3d11c324e1b564ba507a
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-05-27 20:47:06 -0700

    added django lab 2 mob lab and lab 3 updates

--- Code/Troy/Django/labs/todo_list/admin.py
+++ Code/Troy/Django/labs/todo_list/admin.py
@@ -1,6 +1,5 @@
-
 from django.contrib import admin
-from .models import Contacts
-
+from .models import TodoItem
 # Register your models here.
-admin.site.register(Contacts)
+
+admin.site.register(TodoItem)
