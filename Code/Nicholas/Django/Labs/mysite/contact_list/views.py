22f3afdf64a1fdb9643a9c0c2d403b058b97161d
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-05-28 19:26:43 -0400

    lab prog

--- Code/Nicholas/Django/Labs/mysite/contact_list/views.py
+++ Code/Nicholas/Django/Labs/mysite/contact_list/views.py
@@ -9,3 +9,10 @@ def index(request):
     }
     return render(request, 'contact_list/index.html', context)
 
+
+
+def detail(request, contact_id):  
+    contact = get_object_or_404(Contact, pk=contact_id)  
+    if not post.approved:
+        raise Http404
+    return render(request, 'contact_list.detail.html', {'contact': contact})
