b287530bb15f354d2239d52524a8f5bf89bc53cd
Matthew Cooper <flux2341@gmail.com>
2020-05-28 11:50:13 -0700

    models demo

--- Code/Matthew/django/mysite/demo/admin.py
+++ Code/Matthew/django/mysite/demo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Country, CapitalCity, User, City, Author, Book
+
+admin.site.register(Country)
+admin.site.register(CapitalCity)
+admin.site.register(User)
+admin.site.register(City)
+admin.site.register(Author)
+admin.site.register(Book)
