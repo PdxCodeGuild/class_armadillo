b23d146881f835210f404f07f118ef415391d063
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-05-29 16:33:54 -0700

    Contact details page made, creating new and updating now functioning as intended. Only delete functionality, reworking variable names, and styling left.

--- Code/Talieson/django/mysite/contactlist/urls.py
+++ Code/Talieson/django/mysite/contactlist/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create_contact', views.create_contact, name='create_contact'),
     path('', views.contacts, name='contacts'),
     path('contact_details/<int:id>/', views.details, name='details'),
+    path('update_contact/<int:id>/', views.update, name='update_contact'),
 ]
