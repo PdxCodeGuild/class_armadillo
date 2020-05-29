d02fe8fc56a082d2238f339e970f9483395c4213
Samuel Purdy <spurdy885@gmail.com>
2020-05-29 15:50:48 -0700

    Updated lab03

--- Code/Samuel/django/lab03/mysite/contacts/views.py
+++ Code/Samuel/django/lab03/mysite/contacts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from .models import Contact
+import string
 
 def index(request):
     list_of_contacts = Contact.objects.order_by('first_name')
@@ -33,6 +34,24 @@ def contacts_new(request):
 def contacts_new_submit(request):
     print(request.POST)
     cell = 'is_cell' in request.POST
+    error_message = list()
     new_contact = Contact(first_name = request.POST['first_name'], last_name = request.POST['last_name'], age = request.POST['age'], birthday = request.POST['birthday'], phone_number = request.POST['phone_number'], is_cell = cell, comments = request.POST['comments'])
+    if new_contact.first_name == "":
+        error_message.append("First Name field can not be left empty!")
+    if new_contact.last_name == "":
+        error_message.append("First Name field can not be left empty!")
+    if new_contact.age == "":
+        error_message.append("Age field can not be left empty!")
+    if not new_contact.age.isnumeric():
+        error_message.append("Age field can only contain a whole positive number!")
+    if new_contact.birthday == "":
+        error_message.append("Birthday field can not be left empty!")
+    if new_contact.phone_number == "":
+        error_message.append("Phone Number Field can not be left empty!")
+    if len(error_message) != 0:
+        context = {
+            'error_message': error_message
+        }
+        return render(request, 'contacts/new.html', context)
     new_contact.save()
     return HttpResponseRedirect(reverse('contacts:contacts_id', args=(new_contact.id,)))
