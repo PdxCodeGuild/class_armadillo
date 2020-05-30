16c3bfbbf550fca2f71af40dc3e99f668eb1622a
vladimirantigua <vladimirantigua@gmail.com>
2020-05-30 11:02:30 -0700

    lba 3 Django

--- Code/vlad/django_folder/labs/contacts/urls.py
+++ Code/vlad/django_folder/labs/contacts/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'contacts'
 urlpatterns = [
-  path('index/', views.index, name='index')
+  path('', views.index, name='index'),
+  path('<int:contacts_id>/', views.detail, name='detail'),
+  path('new/', views.create_new_contact, name='create_new_contact'),
+  path('save_contact/', views.save_post, name='save_post'),
 ]
--- Code/vlad/django_folder/labs/contacts/views.py
+++ Code/vlad/django_folder/labs/contacts/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Contacts
 
 # def index(request):
 #   return HttpResponse('hello world!') # in order to see hello world I needed to use the following URL: http://localhost:8000/contacts/index/
-from django.shortcuts import render
+
 
 def index(request):
   contacts = Contacts.objects.order_by('last_name') # I need to import model in the view in order to access it = from .models import Contacts
