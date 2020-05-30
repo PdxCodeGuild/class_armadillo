3474b7a509ae24979f5128ae98585f12bfd5e5bc
nellagio <nickgallo41@gmail.com>
2020-05-30 13:27:06 -0700

    fixing coding

--- Code/NGallo/Django_Labs/lab001/mysite/polls/models.py
+++ Code/NGallo/Django_Labs/lab001/mysite/polls/models.py
@@ -6,10 +6,9 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def qas_published_recently(self):
-        return self.pub_date >= timezone.now()
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     def __str__(self):
-
         return self.question_text
     
 
@@ -19,6 +18,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+   
     
