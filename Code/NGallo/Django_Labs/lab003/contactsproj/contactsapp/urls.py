0f46b75f8f53c3a70a7ece1117baa2bb38806ddd
nellagio <nickgallo41@gmail.com>
2020-05-30 22:56:02 -0700

    form complete

--- Code/NGallo/Django_Labs/lab003/contactsproj/contactsapp/urls.py
+++ Code/NGallo/Django_Labs/lab003/contactsproj/contactsapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:contact_id>/', views.detail, name='detail'),
     path('new/', views.addnew, name='new'),
+    path('new/submit/', views.submit_form, name='submit'),
 ]
--- Code/NGallo/Django_Labs/lab003/contactsproj/contactsapp/views.py
+++ Code/NGallo/Django_Labs/lab003/contactsproj/contactsapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 from .models import Contact
 
 
@@ -27,3 +28,24 @@ def detail(request, contact_id):
 def addnew(request):
     context = { }
     return render(request, 'contactsapp/addnew.html', context)
+
+def submit_form(request):
+
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    age = request.POST['age']
+    birthday = request.POST['birthday']
+    phone_number = request.POST['phone_number']
+    is_cell = 'is_cell' in request.POST
+    comment = request.POST['comment']
+
+    contact = Contact.objects.create(first_name = first_name, 
+        last_name = last_name,
+        age = age,
+        birthday = birthday,
+        phone_number = phone_number,
+        is_cell = is_cell,
+        comment = comment,      
+    )
+
+    return HttpResponseRedirect(reverse('contactsapp:detail', args=(contact.id,)))
