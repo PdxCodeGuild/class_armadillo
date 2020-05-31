06333e1fe6c280d5760894898f32d6fec4b1f11a
vladimirantigua <vladimirantigua@gmail.com>
2020-05-30 17:12:10 -0700

    lab03

--- Code/vlad/django_folder/labs/contacts/urls.py
+++ Code/vlad/django_folder/labs/contacts/urls.py
@@ -6,5 +6,5 @@ app_name = 'contacts'
 urlpatterns = [
   path('', views.index, name='index'),
   path('<int:contact_id>/', views.detail, name='detail'),
-  path('new/', views.contact_new, name='create_new_contact'),  
-  path('new/submit/', views.create_new_contact, name='create_new_contact')
+  # path('new/', views.contact_new, name='create_new_contact'),  
+  # path('new/submit/', views.create_new_contact, name='create_new_contact')
