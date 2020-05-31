b3566dbbdcb1f4e1b0f1e2ed4ca8cb3f8be3ac0e
nellagio <nickgallo41@gmail.com>
2020-05-30 19:50:47 -0700

    finish part 3 of tut

--- Code/NGallo/Django_Labs/lab001/mysite/polls/views.py
+++ Code/NGallo/Django_Labs/lab001/mysite/polls/views.py
@@ -1,17 +1,16 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Question
+from django.shortcuts import get_object_or_404, render
 
+from django.http import HttpResponse
+
+from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s. " % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s. "
