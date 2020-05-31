92f723a7624421a33360ebabc6ea91a0bcd32289
Billy Barnett <bbarnett2@gmail.com>
2020-05-31 04:33:33 -0700

    Added lab03 ver 1 (Contacts)

--- Code/Billy/django/mysite/mysite/urls.py
+++ Code/Billy/django/mysite/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('contacts/', include('contacts.urls')),
 ]
