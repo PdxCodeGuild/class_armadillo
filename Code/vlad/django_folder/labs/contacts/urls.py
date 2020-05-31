20df6911da5360b724f90cc7b0184e02cce6f5d0
vladimirantigua <vladimirantigua@gmail.com>
2020-05-30 17:22:29 -0700

    lab3 Contact django adding background

--- Code/vlad/django_folder/labs/contacts/urls.py
+++ Code/vlad/django_folder/labs/contacts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('<int:contact_id>/', views.detail, name='detail'),
   # path('new/', views.contact_new, name='create_new_contact'),  
-  # path('new/submit/', views.create_new_contact, name='create_new_contact')
+  # path('new/submit/', views.create_new_contact, name='create_new_contact'),
+]
