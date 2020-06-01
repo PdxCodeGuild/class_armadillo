a6c773dd105281c2f354028c1dfc85fcc66b26b4
Matthew Cooper <flux2341@gmail.com>
2020-06-01 09:23:38 -0700

    updated pokedex lab

--- Code/Matthew/django/mysite/blogapp/models.py
+++ Code/Matthew/django/mysite/blogapp/models.py
@@ -11,7 +11,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     body = models.TextField()
-    type = models.ForeignKey(BlogPostType, on_delete=models.PROTECT)
+    type = models.ForeignKey(BlogPostType, on_delete=models.PROTECT, related_name='posts')
     created_date = models.DateTimeField()
     approved = models.BooleanField()
 
