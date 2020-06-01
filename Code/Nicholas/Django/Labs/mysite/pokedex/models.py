367316c8dc9012bddd4f5539707c1c25a19db390
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-06-01 19:28:14 -0400

    beginning of Django lab 4

---
+++ Code/Nicholas/Django/Labs/mysite/pokedex/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class PokemonType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Pokemon(models.Model):
+    number = models.IntegerField(max_length=3)
+    name = models.CharField(max_length=50)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    image_front = models.CharField()
+    image_back = models.Charfield()
+    types = models.ManyToManyField(PokemonType)
+
+    def __str__(self):
+        return self.image_front + ' ' + self.name
