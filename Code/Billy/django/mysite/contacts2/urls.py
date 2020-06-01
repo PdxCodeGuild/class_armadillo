611809424073fa2cb24dc51219fad629c247c8db
Billy Barnett <bbarnett2@gmail.com>
2020-06-01 10:54:29 -0700

    *saved progress lab 03 ver 2 (Contacts)

--- Code/Billy/django/mysite/contacts2/urls.py
+++ Code/Billy/django/mysite/contacts2/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='contacts'),
     path('<int:card_id>/', views.detail, name='detail'),
     path('new/', views.entry_page, name='entry_page'),
-    path('submit/', views.submit_contact, name='submit_contact'),
+    path('new/submit/', views.submit_contact, name='submit_contact'),
     path('delete/<int:card_id>/', views.delete, name='delete'),
 ]
