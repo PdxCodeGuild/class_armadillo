1fce7a9eabf919a3374d1ec3e825748844425cdc
Desi0930 <pacifichik24@yahoo.com>
2020-06-01 17:38:49 -0700

    modified

---
+++ Code/Desi/Django/Django1/lab01/blogproj/settings.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blogapp'
+]
