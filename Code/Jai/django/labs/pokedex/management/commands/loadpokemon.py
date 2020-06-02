48f5253ab1ae555510bb8fd4e7c955a1a094846c
jhurdle7 <63259028+jhurdle7@users.noreply.github.com>
2020-06-02 16:33:43 -0700

    getting close

--- Code/Jai/django/labs/pokedex/management/commands/loadpokemon.py
+++ Code/Jai/django/labs/pokedex/management/commands/loadpokemon.py
@@ -1,10 +1,46 @@
 
 from django.core.management.base import BaseCommand
-
-# from .models import Pokemon
+import json
+from pokedex.models import Pokemon, PokemonType
 
 class Command(BaseCommand):
+
+
+    
     def handle(self, *args, **options):
         with open('./pokedex/management/commands/pokemon.json', 'r') as file:
             text = file.read()
-            print(text)
+        data = json.loads(text)
+        for pokemon in data['pokemon']:
+            number = pokemon['number']
+            name = pokemon['name']
+            height = pokemon['height'] 
+            weight = pokemon['weight']
+            image_front = pokemon['image_front'] 
+            image_back = pokemon['image_back'] 
+            types = pokemon['types']
+
+            new_pokemon = Pokemon(number=number,
+                                name=name,
+                                height=height,
+                                weight=weight,
+                                image_front=image_front,
+                                image_back=image_back,)
+            new_pokemon.save()                    
+            for ptype in types:
+                if ptype == types[0]:
+                    p_types1 = PokemonType(name=types)
+                    p_types1.save()
+                elif ptype == types[1]: 
+                    p_types2 = PokemonType(name=types)
+                    p_types2.save()                   
+            
+            if len(types)== 1:
+                new_pokemon.types.add(p_types1) 
+            else:
+                new_pokemon.types.add(p_types1, p_types2)
+
+            # save the contact to the database
+            
+            
+          
--- Code/Jai/django/labs/pokedex/models.py
+++ Code/Jai/django/labs/pokedex/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class Pokemon(models.Model):
-    number = models.IntegerField()
-    name = models.CharField(max_length=200)
-    height = models.IntegerField()
-    weight = models.IntegerField
-    image_front = models.CharField(max_length=200)
-    image_back = models.CharField(max_length=200)
-    types = models.ManyToManyField(pokemontype)
 
-    def __str__return(self):
-        return self.name + ' - ' + self.type
 
 class PokemonType(models.Model):
-    name = models.CharField(max_length=50)
-    
+    name = models.CharField(max_length=100)
+
+    def __str__return(self):
+        return self.number + ' - ' + self.name
+
+class Pokemon(models.Model):
+    number = models.IntegerField(default=0)
+    name = models.CharField(max_length=200)
+    height = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
+    image_front = models.CharField(max_length=200)
+    image_back = models.CharField(max_length=200)
+    types = models.ManyToManyField(PokemonType)
+
+    def __str__return(self):
+        return self.number + ' - ' + self.name
+
