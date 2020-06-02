d85d48749850c60b6620e9a1b9c88f001a7083c0
vladimirantigua <vladimirantigua@gmail.com>
2020-06-02 16:51:17 -0700

    added the load joson the pokemon Lab 4

--- Code/vlad/django_folder/labs/pokedex/urls.py
+++ Code/vlad/django_folder/labs/pokedex/urls.py
@@ -5,4 +5,5 @@ from django.contrib import admin
 app_name = 'pokedex'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pokemon_id>/', views.detail, name='detail'),
 ]
--- Code/vlad/django_folder/labs/pokedex/views.py
+++ Code/vlad/django_folder/labs/pokedex/views.py
@@ -14,3 +14,13 @@ def index(request):
 
     }
     return render(request, 'pokedex/index.html', context)
+
+
+def detail(request, pokedex_id):  # contact_id each person have a unique contact_id
+    contact = get_object_or_404(Pokemon, pk=pokedex_id)
+    # {'contact': contact}) this is giving a single contact this is why is singular because when we click the name of one of the people in the contact list it will give only that person contact details instead of everyone else
+    return render(request, 'pokemon/detail.html', {'pokemon': pokemon})
+
+
+# def create_pokemon_page(request):
+#     return render(request, 'pokemon/new_pokemon.html')
