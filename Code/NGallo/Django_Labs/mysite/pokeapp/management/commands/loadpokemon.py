34e16036c2ca3b54f712913bb49425c8341f2395
nellagio <nickgallo41@gmail.com>
2020-06-02 22:59:32 -0700

    displaying pokemon on page

--- Code/NGallo/Django_Labs/mysite/pokeapp/management/commands/loadpokemon.py
+++ Code/NGallo/Django_Labs/mysite/pokeapp/management/commands/loadpokemon.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
             pokemon_list = dictionary_of_pokemon['pokemon'] # extract the list inside the dictionary
 
             for pokemon in pokemon_list:
-                # getting out the contact data into local variables
+                # getting out the pokemon data variable = dictionary['key']
                 name   = pokemon['name']
                 number    = pokemon['number']
                 height     = pokemon['height']
@@ -19,7 +19,8 @@ class Command(BaseCommand):
                 image_front = pokemon['image_front']
                 image_back      = pokemon['image_back']
                 types     = pokemon['types']
-                # create a contact from our data
+                # create a pokemon from our data
+                # new model instance Pokemon
                 pokemon = Pokemon(name=name,
                                     number=number,
                                     height=height,
@@ -30,12 +31,14 @@ class Command(BaseCommand):
                 # save the contact to the database
                 pokemon.save()
                 
+                # this will go through the types and get them 
                 for type in types:
                     try:
                         poke_type = PokemonType.objects.get(name=type)
-
+                    # if the type does not exist in the database it will create one
                     except PokemonType.DoesNotExist:
                         poke_type = PokemonType.objects.create(name=type)        
-                    
+                    # adding the type to the pokemon
                     pokemon.types.add(poke_type)
+                # must save everytime you alter the database
                 pokemon.save()
