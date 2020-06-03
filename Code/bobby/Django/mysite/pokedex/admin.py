62f5c2f1f6e01c6492478869b0186589fd1151c8
Bobby Wickliffe <ravenhawke5@gmail.com>
2020-06-03 16:43:28 -0700

    Pokedex Trying to figure this out

--- Code/bobby/Django/mysite/pokedex/admin.py
+++ Code/bobby/Django/mysite/pokedex/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Pokemon, PokemonType
 
 # Register your models here.
--- Code/bobby/Django/mysite/pokedex/views.py
+++ Code/bobby/Django/mysite/pokedex/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def pokedex(request):
-    return HttpResponse('Hello Pokemon World')
+def index(request):
+    context = {
+        'message': 'Hello Pokemon World'
+    }
+    return render(request, 'pokedex/index.html', context)
