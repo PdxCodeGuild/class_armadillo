984c3cabad1d3a635ea8ad6383d8bd0242df257a
Samuel Purdy <spurdy885@gmail.com>
2020-06-03 10:22:50 -0700

    Added part 2

--- Code/Samuel/django/lab04/mysite/pokedex/urls.py
+++ Code/Samuel/django/lab04/mysite/pokedex/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'pokedex'
 urlpatterns = [
-    path('home/', views.index, name='home')
+    path('home/', views.index, name='home'),
+    path('<int:pokemon_id>/', views.pokemon_id, name='pokemon_id'),
+    path('pokemon_type/>', views.pokemon_type, name='pokemon_type'),
 ]
--- Code/Samuel/django/lab04/mysite/pokedex/views.py
+++ Code/Samuel/django/lab04/mysite/pokedex/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.db.models import Q
+from .models import Pokemon, Type
 
 def index(request):
-    message = 'this is a test'
+    list_of_pokemon = Pokemon.objects.order_by('number')
+    template = loader.get_template('pokedex/index.html')
     context = {
-        'message': message,
+        'list_of_pokemon': list_of_pokemon,
     }
-    return render(request, 'pokedex/index.html', context)
+    return HttpResponse(template.render(context, request))
+
+def pokemon_id(request, pokemon_id):
+    single_pokemon = Pokemon.objects.filter(pk=pokemon_id).first()
+    template = loader.get_template('pokedex/single.html')
+    context = {
+        'single_pokemon': single_pokemon,
+    }
+    return HttpResponse(template.render(context, request))
+
+def pokemon_type(request):
+    list_of_pokemon = Pokemon.objects.all()
+    for poke in list_of_pokemon:
+        print(poke)
+    template = loader.get_template('pokedex/types.html')
+    context = {
+        'list_of_pokemon': list_of_pokemon,
+    }
+    return HttpResponseRedirect(reverse('pokedex:pokemon_type', args=(list_of_pokemon.id,)))
