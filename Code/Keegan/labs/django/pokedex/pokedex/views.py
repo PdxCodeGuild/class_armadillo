328d48f3208b1edf0a851141a116fb8e9437d35c
dirtTastesGood <keegood8@gmail.com>
2020-06-02 21:16:32 -0700

    format pokemon data and image, search bar works on the back end

--- Code/Keegan/labs/django/pokedex/pokedex/views.py
+++ Code/Keegan/labs/django/pokedex/pokedex/views.py
@@ -3,11 +3,24 @@ from django.shortcuts import render
 from pokemon.models import Pokemon, PokemonType
 
 def pokedex(request):
-    pokemon = Pokemon.objects.last()
-    types = ','.join([typ.name for typ in pokemon.types.all()])
+    if request.method == 'POST':
+        query = request.POST['query']
+        query_type = request.POST['query_type']
+
+        if query_type == 'name':
+            pokemon_list = Pokemon.objects.filter(name__icontains=query)
+        elif query_type == 'poke_type':
+            pokemon_list = Pokemon.objects.filter(types__name__icontains=query)
+        elif query_type == 'number':
+            pass
+        
+        print(pokemon_list)
+    pokemon = Pokemon.objects.get(pk=123)
+    types = ', '.join([typ.name for typ in pokemon.types.all()])
 
     context = {
         'pokemon': pokemon,
         'types': types,
     }
+
     return render(request, 'pokedex.html', context)
