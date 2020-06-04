d8cc44874ef5f6b0e46f6ab883bd114f888d9d2e
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-06-04 09:35:02 -0700

    implementing paginator

--- Code/Talieson/django/mysite/pokedex/views.py
+++ Code/Talieson/django/mysite/pokedex/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import Pokemon, PokemonType
 
 # Create your views here.
 def index(request):
-
+    
+    if request.method =='POST':
+        search = request.POST['search']
+        pokemon = Pokemon.filter(name__icontains=search)
+        context = {"Pokemon": pokemon.objects.order_by('number')}
+    paginator = Paginator(Pokemon, 10)
+    page = paginator.page(1)
+    pokemon = page.object_list
     context = {"Pokemon": Pokemon.objects.order_by('number')}
     return render(request, 'index.html', context)
 
