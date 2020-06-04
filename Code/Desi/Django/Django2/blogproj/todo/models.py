7971a55b6a5b0c2b51241bb15f3d80e6dcb9041c
Desi <pacifichik24@yahoo.com>
2020-06-04 12:10:30 -0700

    pokedex stuff

--- Code/Desi/Django/Django2/blogproj/todo/models.py
+++ Code/Desi/Django/Django2/blogproj/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime
+from django.utils import timezone
 
 # Create your models here.
+class todo(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.IntegerField(nax_length=200)
+    created = models.DateTImeIntegerField(auto_now_add=True)
+    completed = models.DateTimeField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    priority= models.IntegerField(default=0)
