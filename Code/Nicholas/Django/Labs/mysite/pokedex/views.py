604f5f51db34d2b33f1975a21a59a4ddf34c5b49
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-06-04 19:18:28 -0400

    added search

--- Code/Nicholas/Django/Labs/mysite/pokedex/views.py
+++ Code/Nicholas/Django/Labs/mysite/pokedex/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from .models import Pokemon, PokemonType
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import Q
 
 def index(request):
     pokemon = Pokemon.objects.order_by('number')
+    search = ''
+    if request.method == 'POST':
+        search = request.POST['search']
+        pokemon = pokemon.filter(Q(name__icontains=search))
     template = loader.get_template('pokedex/index.html')
     context = {
         'pokemon': pokemon,
+        'search': search
     }
     return HttpResponse(template.render(context, request))
 
