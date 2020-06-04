f0580900dc39adfe8af5412795c1d2df2c59d346
Nicholas Kalinin <nicholasekalinin@gmail.com>
2020-06-04 18:25:06 -0400

    Pokedex v2

--- Code/Nicholas/Django/Labs/mysite/pokedex/views.py
+++ Code/Nicholas/Django/Labs/mysite/pokedex/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Pokemon, PokemonType
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
-    pokemon = Contact.objects.order_by('number')
+    pokemon = Pokemon.objects.order_by('number')
     template = loader.get_template('pokedex/index.html')
     context = {
         'pokemon': pokemon,
     }
     return HttpResponse(template.render(context, request))
+
+
