392f1b5834aa373ce30d579e8bc86600574b54cf
seanyboy1 <infidel12@hotmail.com>
2020-06-11 12:08:39 -0700

    updated java labs

--- Code/Charlie/javascript/lab1/password generator/password_generator.js
+++ Code/Charlie/javascript/lab1/password generator/password_generator.js
@@ -1,6 +1,5 @@
 let user_input = document.querySelector('#user_input')
 let btn_submit = document.querySelector('#btn_submit')
-let div_output = document.querySelector('#div_output')
 
 function passwordGenerator(letters){
     let random_pg = Math.floor(Math.random()* letters.length)
@@ -8,16 +7,14 @@ function passwordGenerator(letters){
 }
 btn_submit.addEventListener('click', function(){
     let user_input2 = user_input.value
-    let submit2 = btn_submit.value
     let alpha = "abcdefghijklmnopqrstuvwxyz"
     let numbers = "0123456789"
     let spec_chars = "!@#$%^&*"
 
-    // let user_input = parseInt(prompt("How long do you want your password to be?: "))
-
     let password = ""
-    for (let i = 0; i<user_input; ++i){
-        password += passwordGenerator(alpha + numbers + spec_chars)
+    for (let i=0; i<user_input2; ++i){
+        let x = passwordGenerator(alpha + numbers + spec_chars)
+        password += x
     }
     console.log(password)
     alert(`Your password is: ${password}`)
