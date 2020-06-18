3e1574b72f30773dd97e73844e1f0d86397c5e81
Adam Meier <meieradamn@gmail.com>
2020-06-17 23:29:54 -0700

    Flask lab1 v1 complete / updates to labs

--- Code/Adam/Flask/lab01_flask_intro/app.py
+++ Code/Adam/Flask/lab01_flask_intro/app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import random
+import string
+
+
 app = Flask(__name__)
 
-@app.route('/')
+# path to the view
+@app.route('/', methods=["GET", "POST"])
+# defines our data
 def index():
-    return 'Hello World!'
+
+    output = ''
+
+    if request.method == "POST":
+
+        pw_length = int(request.form['num_select'])
+
+        alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+        i = 0
+
+        while i < pw_length:
+            output += random.choice(alphabet)
+            i += 1
+        
+    # else:???
+
+
+    return render_template('index.html', password = output)
