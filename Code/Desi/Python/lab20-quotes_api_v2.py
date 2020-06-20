d6f530fe9ba11cea93080e4deec594d54ccc4391
Desi <pacifichik24@yahoo.com>
2020-06-19 18:16:46 -0700

    updates

--- Code/Desi/Python/lab20-quotes_api_v2.py
+++ Code/Desi/Python/lab20-quotes_api_v2.py
@@ -6,6 +6,6 @@ keyword = input("enter a keyword to search for quotes: ")
 # make formatted string to use lines 6 for category
 url = f'https://favqs.com/api/quotes?page=1&filter={keyword}'
 
-headers = {"Authorization": 'token token="855df50978dc9afd6bf86579913c9f8b"'}
+headers = {"Authorization": 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
 response = requests.get(url, headers=headers)
 print(response.text)
