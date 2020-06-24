0db15deabe1c3b667dc6562588b64c861332a7c6
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-06-23 22:00:48 -0700

    fixed code for contacts django lab

--- Code/Troy/Django/labs/contacts/models.py
+++ Code/Troy/Django/labs/contacts/models.py
@@ -10,10 +10,13 @@ class Contacts(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField()
-    birth_date = models.DateField(default="YYYY-MM-DD")
+    birth_date = models.DateField()
     phone_number = PhoneField(blank=True, help_text='Contact phone number')
     is_cell = models.BooleanField()
     comments = models.TextField(blank=True)
+
+    def format_bday (self):
+        return self.birth_date.strftime('%Y-%m-%d')
 
     def __str__(self):
         return self.last_name + ', ' + self.first_name
