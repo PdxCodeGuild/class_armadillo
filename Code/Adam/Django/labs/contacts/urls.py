3f5dceda2b6aa9dd1d8f7c485c87ac7eba05870f
Adam Meier <meieradamn@gmail.com>
2020-07-29 15:02:51 -0700

    updates

--- Code/Adam/Django/labs/contacts/urls.py
+++ Code/Adam/Django/labs/contacts/urls.py
@@ -5,4 +5,7 @@ app_name = 'contacts'
 urlpatterns = [
   path('base/', views.base, name='base'),
   path('', views.index, name='index'),
+  path('<int:contact_id>/', views.detail, name='detail'),
+  path('new/', views.new, name='new'),
+  path('new/save/', views.new_save, name='new_save'),
 ]
--- Code/Adam/Django/labs/contacts/views.py
+++ Code/Adam/Django/labs/contacts/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-
-# Create an index view
 from django.http import HttpResponse
+
+from .models import Contact
 
 def base(request):
     return render(request, 'contacts/base.html')
 
 
 def index(request):
-    context = {}
+
+    contacts = Contact.objects.all().order_by('last_name')
+    # print(contacts)
+    context = {
+        'contacts': contacts
+    }
     return render(request, 'contacts/index.html', context)
+
+
+def detail(request, contact_id):
+    return HttpResponse('detail page for contact with id ' + str(contact_id))
+
+
+def new(request):
+    return HttpResponse('new contact page')
+
+
+def new_save(request):
+    return HttpResponse('save contact')
