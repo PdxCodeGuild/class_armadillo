b0d832c7ba65ef52fddb881d9592aaee946acaad
Adam Meier <meieradamn@gmail.com>
2020-07-29 18:25:14 -0700

    Django Lab 3 v1 complete

--- Code/Adam/Django/labs/contacts/views.py
+++ Code/Adam/Django/labs/contacts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Contact
+
 
 def base(request):
     return render(request, 'contacts/base.html')
@@ -30,4 +31,21 @@ def new(request):
 
 
 def new_save(request):
-    return HttpResponse('save contact')
+    print(request.POST)
+    contact_first_name = request.POST['contact_first_name']
+    contact_last_name = request.POST['contact_last_name']
+    contact_birthday = request.POST['contact_birthday']
+    contact_phone_number = request.POST['contact_phone_number']
+    contact_is_cell = 'contact_is_cell' in request.POST
+    contact_comments = request.POST['contact_comments']
+
+    contact = Contact(first_name=contact_first_name,
+                      last_name=contact_last_name,
+                      birthday=contact_birthday,
+                      phone_number=contact_phone_number,
+                      is_cell=contact_is_cell,
+                      comment=contact_comments)
+
+    contact.save()
+
+    return HttpResponseRedirect(reverse('contacts:detail', args=[contact.id,]))
