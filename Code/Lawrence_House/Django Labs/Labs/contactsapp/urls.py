ab1332fd1f4e465d5b46d5b5b84843ae01e8bfa2
Lawrence <trehouse650@gmail.com>
2020-07-30 13:28:54 -0800

    added contacts, detail, new contact page

--- Code/Lawrence_House/Django Labs/Labs/contactsapp/urls.py
+++ Code/Lawrence_House/Django Labs/Labs/contactsapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'contactsapp'
 urlpatterns = [
-  path('index/', views.index, name='index')
+  path('index/', views.index, name='index'),
+  path('<int:id>/', views.detail, name='detail'),
+  path('new/', views.contactsnew, name='contactsnew'),
+  # path('contacts/new/submit/', views.submit, name='submit'),
 ]
